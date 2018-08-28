67d642208bf1378cae8d178028c18f11b7dc16ea
shreyatpandey <arjenbhar@gmail.com>
2018-08-27 22:14:16 -0700

    transposematrix.cpp

---
+++ transposematrix.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+   public:
+	vector<vector<int>>transpose(vector<vector<int>>&A)
+	{
+	 //2-D vector to store the result
+          vector<vector<int>>result(A.size());
+	  size_t row

6bdf9b8587c1df806f3578551b979f8b311d2fa4
shreyatpandey <arjenbhar@gmail.com>
2018-10-09 18:49:11 -0700

    kth_smallest_element.cpp

--- kth_smallest_element.cpp
+++ kth_smallest_element.cpp
@@ -4,7 +4,7 @@ using namespace std;
 class Solution
 {
 	public:
-	int kthsmallestelementmatrix(vector<vector<int>>&input,int k)
+	int kthsmallestelementmatrix_binarysearch(vector<vector<int>>&input,int k)
 	{
 	  uint32_t length = input.size();
 	  size_t begin_element = input[0][0];
@@ -20,6 +20,27 @@ class Solution
 	}
 	return begin_element;
 	}
+	int kthsmallestelementmatrix_priorityqueue(vector<vector<int>>&input,int k)
+	{
+	  int length = input.size();
+	//  int row = 0;
+	 // int cols = 0;
+	  priority_queue<int,vector<int>,greater<int>>pq; //min-heap
+	  for(int row=0;row<length;row +=1 )
+	  {
+	   for(int cols= 0;cols<length;cols +=1 )
+		{
+		  pq.push(input[row][cols]);
+		
+	}
+	}
+	while(--k)
+	{
+	// k -=1;
+	  pq.pop();
+	}
+	return pq.top();
+	}
 
 };
 
@@ -28,7 +49,9 @@ int main()
 {
   vector<vector<int>>input{{1,5,9},{10,11,13},{12,13,15}};
   Solution s;
-  cout<<"kth_smallest_element_sorted_matrix:"<<s.kthsmallestelementmatrix(input,8)<<endl; 
+  cout<<"kth_smallest_element_sorted_matrix_binarysearch:"<<s.kthsmallestelementmatrix_binarysearch(input,8)<<endl;
+  cout<<"kth_smallest_element_sorted_matrix_priorityqueue:"<<s.kthsmallestelementmatrix_priorityqueue(input,8)<<endl;
+	 
   return 0;
 }
 

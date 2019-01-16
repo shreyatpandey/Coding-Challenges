6af0d145b298cc65f437144c37ceaf5839cb88eb
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2019-01-15 17:46:22 -0800

    Update twosum.cpp

--- twosum.cpp
+++ twosum.cpp
@@ -20,23 +20,23 @@ class Solution
     }
   vector<int>twosum_2(vector<int>&nums,int target)
   {
-   unordered_map<int,int>hash_map;
-			vector<int>return_1;
-			for(int i=0;i<nums.size();i++)
-			{
-				hash_map.emplace(i,nums[i]);
-			}
+	unordered_map<int,int>hash_map;
+	vector<int>return_1;
+	for(int i=0;i<nums.size();i++)
+	{
+		hash_map.emplace(i,nums[i]);
+	}
 
-			for(int j=0;j<nums.size();j++)
-			{
-				int result = target - nums[j];
-			if(hash_map.find(result) != hash_map.end())
-				{		
-				return_1.push_back(hash_map[nums[j]]);
-				return_1.push_back(j);
-				return return_1;
-			}
-  }
+	for(int j=0;j<nums.size();j++)
+	{
+		int result = target - nums[j];
+		if(hash_map.find(result) != hash_map.end())
+		{		
+		return_1.push_back(hash_map[nums[j]]);
+		return_1.push_back(j);
+		return return_1;
+		}
+	}
    return return_1;
   }
  };

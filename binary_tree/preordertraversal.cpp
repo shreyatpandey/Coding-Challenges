1145dfc1a8067cff45cebe4a05f508ac1e1ff91d
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2019-05-14 18:24:32 -0700

    Update preordertraversal.cpp

--- binary_tree/preordertraversal.cpp
+++ binary_tree/preordertraversal.cpp
@@ -1,7 +1,15 @@
 class Solution {
+   private:
+        void preorderTraversal_output(TreeNode* root, vector<int>&preorderTraversal_ret)
+        {
+            if(root == NULL)
+                return;
+            preorderTraversal_ret.push_back(root->val);
+            preorderTraversal_output(root->left,preorderTraversal_ret);
+            preorderTraversal_output(root->right,preorderTraversal_ret);
+        }
 public:
-   
-    vector<int> preorderTraversal(TreeNode* root) {
+    vector<int> preorderTraversal_recursion(TreeNode* root) {
          vector<int>preorderTraversal_ret;
         if(root == NULL)
             return preorderTraversal_ret;
@@ -10,13 +18,27 @@ public:
         return preorderTraversal_ret;
         
     }
-    private:
-        void preorderTraversal_output(TreeNode* root, vector<int>&preorderTraversal_ret)
+   vector<int> preorderTraversal_stack(TreeNode* root)
+   {
+       vector<int>preorderTraversal_value;
+        if(root == NULL)
+            return preorderTraversal_value;
+        stack<TreeNode*>preorderTraversal_stack;
+        preorderTraversal_stack.emplace(root);
+        while(!preorderTraversal_stack.empty())
         {
-            if(root == NULL)
-                return;
-            preorderTraversal_ret.push_back(root->val);
-            preorderTraversal_output(root->left,preorderTraversal_ret);
-            preorderTraversal_output(root->right,preorderTraversal_ret);
-        }   
+            TreeNode* preorderTraversal_node = preorderTraversal_stack.top();
+            preorderTraversal_value.emplace_back(preorderTraversal_node->val);
+            preorderTraversal_stack.pop();
+            
+            if(preorderTraversal_node->right)
+                preorderTraversal_stack.emplace(preorderTraversal_node->right);
+            if(preorderTraversal_node->left)
+                preorderTraversal_stack.emplace(preorderTraversal_node->left);
+            
+        }
+        return preorderTraversal_value;  
+   }
+    
+      
 };

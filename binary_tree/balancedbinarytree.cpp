5bd5c23380248a8cd53806b82736d69b21802e22
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2019-05-18 12:46:50 -0700

    Update balancedbinarytree.cpp

--- binary_tree/balancedbinarytree.cpp
+++ binary_tree/balancedbinarytree.cpp
@@ -1,3 +1,17 @@
+/*Given a binary tree, determine if it is height-balanced.
+For this problem, a height-balanced binary tree is defined as:
+A binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+Example 1:
+
+Given the following tree [3,9,20,null,null,15,7]:
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 struct TreeNode

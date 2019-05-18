6b0fa77f12852f2cae212e53b7e5ef5f12801edb
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2019-05-18 12:42:19 -0700

    Update lca_bst.cpp

--- binary_tree/lca_bst.cpp
+++ binary_tree/lca_bst.cpp
@@ -3,6 +3,10 @@
 According to the definition of LCA on Wikipedia: 
 “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
 Given binary search tree:  root = [6,2,8,0,4,7,9,null,null,3,5]*/
+/* BST properties:-
+  1. Left subtree of a node N contains nodes whose values are lesser than or equal to node N's value.
+  2. Right subtree of a node N contains nodes whose values are greater than node N's value.
+  3. Both left and right subtrees are also BSTs. */
 /*Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
 Output: 6
 Explanation: The LCA of nodes 2 and 8 is 6.

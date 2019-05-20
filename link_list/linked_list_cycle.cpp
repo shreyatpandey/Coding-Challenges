907a1f0d7fac6188a75ec99ed2a3a4a61924fd2d
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2019-05-19 19:14:03 -0700

    Update linked_list_cycle.cpp

--- link_list/linked_list_cycle.cpp
+++ link_list/linked_list_cycle.cpp
@@ -34,3 +34,21 @@ public:
         return false;
     }
 };
+
+class Solution_2 {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head == NULL || head->next == NULL)
+            return false;
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        while(slow != fast)
+        {
+            if(fast == NULL || fast->next == NULL)
+                return false;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return true;
+    }
+};

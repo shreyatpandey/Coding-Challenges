b7d0cd2dcc82202e2648e555212feb2f2558a379
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2019-05-30 19:22:53 -0700

    house_robber.cpp

--- dp/house_robber.cpp
+++ dp/house_robber.cpp
@@ -18,3 +18,38 @@ public:
         
     }
 };
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.size() == 0)
+            return 0;
+        if (nums.size() == 1)
+            return nums[0];
+        if (nums.size() == 2)
+            return max(nums[0],nums[1]);
+        vector<int>dp;
+        for(int x:nums)
+        {
+            dp.emplace_back(x);
+        }
+        int global_maxima = 0;
+        for (int i=1;i<nums.size();i++)
+        {
+            int local_maxima = 0;
+            for(int j=0;j<i;j++)
+            {
+                if (abs(j-i) != 1)
+                {
+                    local_maxima = dp[j] + dp[i];
+                    global_maxima = max(local_maxima,global_maxima);
+                }
+            }
+            if ( global_maxima < nums[i])
+                global_maxima = nums[i]; 
+            if (global_maxima > dp[i])
+                dp[i] = global_maxima;
+        }
+        return global_maxima;
+    
+    }
+};

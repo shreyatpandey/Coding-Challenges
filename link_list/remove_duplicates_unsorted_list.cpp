36762b02344b96f4e1c05af629e9131107071987
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2019-06-07 19:38:38 -0700

    Update remove_duplicates_unsorted_list.cpp

--- link_list/remove_duplicates_unsorted_list.cpp
+++ link_list/remove_duplicates_unsorted_list.cpp
@@ -1,4 +1,6 @@
-class Solution {
+/*O(N) : Run-Time */
+class Solution 
+{
 public:
     ListNode* deleteDuplicates(ListNode* head) {
         set<int>check_duplicate;
@@ -20,5 +22,31 @@ public:
         }
         return head;
         
+    }
+};
+
+/*O(N^2) : Run-Time */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* temp = head;
+        while(temp != NULL)
+        {
+            ListNode* previous = temp;
+            while(previous && previous->next)
+            {
+                if(previous->next->val == temp->val)
+                {
+                    previous->next = previous->next->next;
+                }
+                else
+                {
+                    previous = previous->next;
+                
+                }
+            }
+            temp = temp->next;
+        }
+        return head;
     }
 };

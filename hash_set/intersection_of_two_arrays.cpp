34abaf4bc7dfb67104e7618f7ba161916ad1a105
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2019-06-21 18:03:15 -0700

    intersection_of_two_arrays.cpp

---
+++ hash_set/intersection_of_two_arrays.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) 
+    {
+        set<int>check_intersection;
+        vector<int>result;
+        for(int i=0;i<nums1.size();i++)
+        {
+            check_intersection.emplace(nums1[i]);
+        }
+        for(int i=0;i<nums2.size();i++)
+        {
+            if (check_intersection.find(nums2[i]) != check_intersection.end())
+            {
+                if ( !(find(result.begin(),result.end(),nums2[i]) != result.end()))
+                    result.emplace_back(nums2[i]);
+            }
+        }
+        return result;
+        
+    }
+};
+/*99.62 % faster */

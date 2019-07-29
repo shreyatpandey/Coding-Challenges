1f46c05443b3bfb26680a9b70654ea371bc7347b
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2019-07-29 10:41:50 -0700

    Update valid_parentheses.cpp

--- valid_parentheses.cpp
+++ valid_parentheses.cpp
@@ -1,3 +1,4 @@
+//Approach-1:
 class Solution {
 public:
     bool isValid(string s) {
@@ -44,4 +45,72 @@ public:
         else
             return true;
     }
+};
+
+//Approach 2: Stack 100% fast
+class Solution 
+{
+public:
+    bool isValid(string s) 
+    {
+        if(s.length() == 1|| s.length()%2!=0)
+        {
+            return false;
+        }
+        if(s.length() == 0)
+            return true;
+        stack<char>st;
+        int i = 0;
+        int flag = 0;
+        while(i<s.length())
+        {
+            if(s[i] == '('||s[i] == '[' || s[i] == '{')
+            {st.push(s[i]);}
+            if(st.size() == 0)
+            {
+                return false;
+            }
+            if(s[i] == ')')
+            {
+                char temp = st.top();
+                st.pop();
+                if(temp != '(')
+                {
+                    flag = 1;
+                    break;
+                }
+                
+            }
+             if(s[i] == ']')
+            {
+                char temp = st.top();
+                st.pop();
+                if(temp != '[')
+                {
+                    flag = 1;
+                    break;
+                }
+                
+            }
+              if(s[i] == '}')
+            {
+                char temp = st.top();
+                st.pop();
+                if(temp != '{')
+                {
+                    flag = 1;
+                    break;
+                }
+                
+            }
+            i += 1;
+            
+            
+        }
+        if (st.size() != 0 || flag == 1)
+            return false;
+        
+        return true; 
+    }
+       
 };

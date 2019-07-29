5049e47211d3128d2c05202e0105792fb555d90e
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2019-07-28 18:14:11 -0700

    Update missing_number.cpp

--- hash_set/missing_number.cpp
+++ hash_set/missing_number.cpp
@@ -1,3 +1,9 @@
+/*Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing from the array.
+Example 1:
+
+Input: [3,0,1]
+Output: 2*/   
+
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {

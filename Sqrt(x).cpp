bf088ac44567ee99de262b187f6e4c023cf51b76
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2019-08-04 10:06:24 -0700

    Sqrt(x)

---
+++ Sqrt(x).cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int mySqrt(int x) 
+    {
+        if (x == 0 || x == 1) 
+          return x; 
+  
+    long int i = 1, result = 1; 
+    while (result <= x) 
+    { 
+      i++; 
+      result = i * i; 
+    } 
+    return i - 1; 
+           
+    }
+
+};

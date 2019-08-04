69160fee9b7ad6358bcd9a14ee3af0ea96bf0c47
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2019-08-04 10:15:57 -0700

    Best Time to Buy and Sell Stock II

---
+++ Best Time to Buy and Sell Stock II.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+	  if (!prices.size())
+	 {  return 0;}
+	  for(int i=1;i<prices.size();i++)
+	  {
+		if(prices[i-1] < prices[i])
+		{
+	
+		 profit += prices[i] - prices[i-1];
+		}
+      }
+        
+ 	return profit;
+ 	}
+        
+    
+};

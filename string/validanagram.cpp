0538debc4d24e2137531e880a9ac859985e392f6
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2019-08-06 15:22:18 -0700

    Update validanagram.cpp

--- string/validanagram.cpp
+++ string/validanagram.cpp
@@ -18,3 +18,28 @@ class Solution
 
 };
 
+
+
+/*single-hash map */
+
+bool isAnagram(char * s, char * t)
+{
+    int frequency_count[27] = {0};
+    for(int i=0;i<strlen(s);i++)
+    {
+        frequency_count[s[i]-'a']++;
+    }
+    for(int i=0;i<strlen(t);i++)
+    {
+        frequency_count[t[i]-'a']--;
+    }
+    for(int i=0;i<26;i++)
+    {
+        if(frequency_count[i] != 0)
+            return false;
+    }
+    return true;
+
+}
+
+

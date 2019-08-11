570dfb8d91000d8ccb49bdf883b903c60eae6349
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2019-08-10 18:55:32 -0700

    Update reorder_list.cpp

--- link_list/reorder_list.cpp
+++ link_list/reorder_list.cpp
@@ -32,3 +32,61 @@ public:
         cur->next = nullptr;
     }
 };
+
+
+//Approach-2
+class Solution {
+    private:
+        ListNode* reverse_link_list(ListNode* start)
+        {
+            ListNode* current = start;
+            ListNode* previous = NULL;
+            ListNode* temp;
+            while(current)
+            {
+                temp = current->next;
+                current->next = previous;
+                previous = current;
+                current = temp;
+            }
+            start = previous;
+            return start;
+        }
+public:
+    void reorderList(ListNode* head) {
+        if (head == NULL)
+            return;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while(fast && fast->next)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* first_part = head;
+        ListNode* second_part = slow->next;
+        slow->next = NULL;
+        second_part = reverse_link_list(second_part);
+        ListNode* dummy_node = new ListNode(0);
+        ListNode* temp = dummy_node;
+        while(first_part || second_part)
+        {
+            if(first_part)
+            {
+                temp->next = first_part;
+                temp = temp->next;
+                first_part = first_part->next;
+            }
+            if(second_part)
+            {
+                temp->next = second_part;
+                temp = temp->next;
+                second_part = second_part->next;
+            }
+            
+        }
+        dummy_node = dummy_node->next;
+        
+        
+    }
+};

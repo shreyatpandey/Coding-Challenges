4526de35dda3d1fb4d7b68873efae5fdade77cd3
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2019-09-16 22:23:01 -0700

    Single Number III.cpp

---
+++ hash_set/Single Number III.cpp
@@ -0,0 +1,38 @@
+/*
+Given an array of numbers nums, in which exactly two elements appear only once and all the other elements appear exactly twice.
+Find the two elements that appear only once.
+
+Example:
+
+Input:  [1,2,1,3,2,5]
+Output: [3,5]
+
+*/
+class Solution 
+{
+        private:
+            map<int,int>frequencyofeachnumber;
+            vector<int>storeresult;
+            
+public:
+    vector<int> singleNumber(vector<int>& nums) 
+    {
+        for(auto num:nums)
+        {
+             if (frequencyofeachnumber.find(i) != frequencyofeachnumber.end())
+            {
+                frequencyofeachnumber[num]++;
+            }
+            else
+            {
+                frequencyofeachnumber[num] = 1;
+            }
+        }
+        for(auto num:nums)
+        {
+            if(frequencyofeachnumber[num] == )
+        }
+        
+        
+    }
+};

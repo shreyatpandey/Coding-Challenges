6b0e4f687b9295d146a0aa47abb90e9730923975
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2019-10-03 14:01:36 -0700

    Group Anagrams.cpp

--- string/Group Anagrams.cpp
+++ string/Group Anagrams.cpp
@@ -1,3 +1,4 @@
+//Complexity :- O(NKlogK)
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
@@ -11,5 +12,38 @@ public:
         }
         for(auto &x: hash_map) ans.push_back(x.second);
         return ans;
+    }
+};
+
+//Complexity: O(NK)
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        map<string,vector<string>>answer;
+        vector<vector<string>>result;
+        vector<int>count(26);
+        for(int i=0;i<strs.size();i++)
+        {
+            //memset(count,0,27); //this did not work
+            fill(count.begin(),count.end(),0);
+            string temp = strs[i];
+            for(int j=0;j<temp.length();j++)
+            {   count[temp[j]-'a']++; }
+            string sb = "";
+            for(int k=0;k<26;k++)
+            {
+                sb.append(":");
+                sb.append(to_string(count[k]));
+            }
+            
+                answer[sb].push_back(strs[i]);
+           
+        }
+        for(auto &x: answer)
+        {
+            result.push_back(x.second);
+        }
+        return result;
+        
     }
 };

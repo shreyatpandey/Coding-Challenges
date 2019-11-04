d0ef14a9a4461ebea26ea0cf8d936b4a95e229a9
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2019-11-03 18:33:46 -0800

    Pow(x,n)

---
+++ Pow(x, n).cpp
@@ -0,0 +1,29 @@
+/*
+Implement pow(x, n), which calculates x raised to the power n (xn).
+
+Example 1:
+
+Input: 2.00000, 10
+Output: 1024.00000
+*/
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
+        }
+        double ans = 1;
+        double current_product = x;
+        for (long long i = N; i>0 ; i /= 2) 
+        {
+            if ((i % 2) == 1) 
+            {
+                ans = ans * current_product;
+            }
+            current_product = current_product * current_product;
+        }
+        return ans;
+    }
+};

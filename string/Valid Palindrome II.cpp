d0c96a4e4d9119ca35f8090ec455e651d2656ee4
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2020-04-04 15:46:33 -0700

    Valid Palindome II

--- string/Valid Palindrome II.cpp
+++ string/Valid Palindrome II.cpp
@@ -28,3 +28,32 @@ bool Solution :: CheckPalindrome ( string input)
     return true ;
     
 }
+
+//Works - faster than 86.33 %
+class Solution {
+    private:
+        bool CheckPalindrome ( string s,int i, int j ) ;
+public:
+    bool validPalindrome(string s) {
+        for (int i = 0; i < s.length() / 2; i++)
+        {
+            if (s.at(i) != s.at (s.length() - 1 - i) ) 
+            {
+                int j = s.length() - 1 - i;
+                return (CheckPalindrome (s, i+1, j) ||
+                        CheckPalindrome (s, i, j-1));
+            }
+        }
+        return true;
+    }
+};
+
+bool Solution::CheckPalindrome( string input, int i, int j)
+{
+   for (int k = i; k <= i + (j - i) / 2; k++) {
+            if (input[k] != input[j - k + i]) 
+                return false;
+        }
+        return true;
+    
+}

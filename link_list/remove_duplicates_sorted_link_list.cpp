1df74782ad39ae5626c9227588329ba483aedda5
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2020-07-05 18:01:52 -0700

    Update remove_duplicates_sorted_link_list.cpp

--- link_list/remove_duplicates_sorted_link_list.cpp
+++ link_list/remove_duplicates_sorted_link_list.cpp
@@ -26,3 +26,25 @@ public:
         
     }
 };
+
+
+//Approach-2
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+          if(head == NULL)
+            return head;
+        ListNode* temp = head;
+        while(temp!=NULL && temp->next != NULL)
+        {
+            while(temp->next && temp->val == temp->next->val) 
+            {
+                temp->next = temp->next->next;
+            }
+            temp = temp->next;
+            
+        }
+        return head;
+        
+    }
+};

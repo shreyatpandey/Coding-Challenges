edc46f3e2c5bc1f99d19fef3cb98b2fbfeac1db8
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2020-07-21 21:39:25 -0700

    Update deletenodeposition.cpp

--- link_list/deletenodeposition.cpp
+++ link_list/deletenodeposition.cpp
@@ -18,3 +18,79 @@ public:
         
     }
 };
+
+/* New way approach */
+#include <stdio.h> 
+#include <stdlib.h> 
+  
+// A linked list node 
+struct Node 
+{ 
+    int data; 
+    struct Node *next; 
+}; 
+  
+/* Given a reference (pointer to pointer) to the head of a list 
+   and an int, inserts a new node on the front of the list. */
+void push(struct Node** head_ref, int new_data) 
+{ 
+    struct Node* new_node = (struct Node*) malloc(sizeof(struct Node)); 
+    new_node->data  = new_data; 
+    new_node->next = (*head_ref); 
+    (*head_ref)    = new_node; 
+} 
+  struct Node* ReturnPreviousNode(struct Node* head,int key)
+{
+    struct Node* temp = head;
+    struct Node* previous = NULL;
+    while(temp)
+    {
+        previous = temp;
+        if(temp->next->data == key)
+        {
+            return previous;
+        }
+        temp = temp->next;
+    }
+    return NULL;
+}
+  
+  
+void deleteNode(struct Node* head,int key)
+{
+    struct Node* Result = ReturnPreviousNode(head,key);
+    if ( Result == NULL)
+        return NULL;
+     Result->next = Result->next->next;
+    
+}
+
+// This function prints contents of linked list starting from  
+// the given node 
+void printList(struct Node *node) 
+{ 
+    while (node != NULL) 
+    { 
+        printf(" %d ", node->data); 
+        node = node->next; 
+    } 
+} 
+  
+/* Drier program to test above functions*/
+int main() 
+{ 
+    /* Start with the empty list */
+    struct Node* head = NULL; 
+  
+    push(&head, 7); 
+    push(&head, 1); 
+    push(&head, 3); 
+    push(&head, 2); 
+  
+    puts("Created Linked List: "); 
+    printList(head);
+    puts("\nDelete Node Linked List with key 1: ");
+    deleteNode(head,1);
+    printList(head); 
+    return 0; 
+}

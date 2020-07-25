74541c669bd6c356cf8cc6d488aa9df43a90e239
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2020-07-25 12:42:03 -0700

    Closest Binary Search Tree Value

--- binary_tree/Closest Binary Search Tree Value.cpp
+++ binary_tree/Closest Binary Search Tree Value.cpp
@@ -18,9 +18,10 @@ Input: root = [4,2,5,1,3], target = 3.714286
 Output: 4
 */
 
-/* Approach -1
+/* Approach -1 //Recursive + Iterative
 TC:- O(N) - linear seach for comparasion
 SC:- O(N) 
+Total Running Time:- 95.17% faster
 */
 class Solution {
     private:
@@ -42,3 +43,26 @@ void Solution :: InorderTraversal ( vector<int>&InorderVector,TreeNode* root)
     InorderTraversal(InorderVector,root->right);
     
 }
+
+/* Approach - 2 // Recursive
+TC:- O(N) or O(h)
+SC:- O(1)
+Total Running Time:- 48.17% faster
+*/
+class Solution {
+    private:
+        int Result = INT_MAX;
+public:
+    int closestValue(TreeNode* root, double target) {
+        if ( root == NULL )
+            return Result;
+        double current = (double)root->val;
+        if ( current == target)
+            return root->val;
+      if( Result ==INT_MAX || abs(Result-target)> abs(current-target))
+            Result = current; 
+        return ( current>target ? closestValue(root->left,target) : closestValue(root->right,target));
+    }
+    
+};
+

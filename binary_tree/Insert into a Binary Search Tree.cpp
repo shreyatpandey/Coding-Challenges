6745038b39cc8b81ff66b77f6d686e4657238d1c
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2020-07-25 18:08:41 -0700

    Update Insert into a Binary Search Tree.cpp

--- binary_tree/Insert into a Binary Search Tree.cpp
+++ binary_tree/Insert into a Binary Search Tree.cpp
@@ -1,3 +1,20 @@
+/* Recursive*/
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        
+        if ( root == NULL )
+            return new TreeNode(val) ;
+        else if ( val < root->val)
+            root->left = insertIntoBST(root->left,val);
+        else
+            root->right = insertIntoBST(root->right,val);
+        return root;
+    }
+};
+
+
+/* Iterative*/
 class Solution 
 {
     private:

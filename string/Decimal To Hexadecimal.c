20b5970124bd6aa79470bef6d660c46b7ea5d125
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2020-07-27 11:41:15 -0700

    Create Decimal To Hexadecimal.c

---
+++ string/Decimal To Hexadecimal.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#define Max 25600
+
+int main()
+{
+  char HexaDecimal[MAX];
+  int InputVal = 125;
+  int HexaDecimalLength = 0;
+  while(InputVal)
+  {
+    int remainder = InputVal % 16;
+    InputVal /= 16;
+    if ( remainder > 9)
+    {
+      HexaDecimal[HexaDecimalLength++] = remainder + 55 ; //(int)'a' - 10
+    }
+    else
+    {
+      HexaDecimal[HexaDecimalLength++] =  remainder + 48 ; //(int)'48'
+    }
+  
+  }
+HexaDecimal[HexaDecimalLength] = '\0';
+return 0;
+}

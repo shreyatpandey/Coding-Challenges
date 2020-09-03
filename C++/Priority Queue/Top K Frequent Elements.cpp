696db7a40036ac4678a7f1c0cb1ed277a70ab113
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2020-09-02 19:11:51 -0700

    Top K Frequent Elements

---
+++ C++/Priority Queue/Top K Frequent Elements.cpp
@@ -0,0 +1,54 @@
+/*
+iven a non-empty array of integers, return the k most frequent elements.
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+Example 2:
+
+Input: nums = [1], k = 1
+Output: [1]
+Note:
+
+You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+It's guaranteed that the answer is unique, in other words the set of the top k frequent elements is unique.
+You can return the answer in any order.
+*/
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        map<int,int>hash_map;
+        vector<int>kmostfrequent_vector;
+        if(nums.size() == 0)
+        {
+            return kmostfrequent_vector; 
+        }
+        //find the frequency of each element
+        for(int i=0;i<nums.size();i++)
+        {
+            hash_map.emplace(nums[i],0);
+            auto it = hash_map.find(nums[i]);
+            ++it->second;
+            hash_map.emplace(nums[i],it->second);
+            
+        }
+        /*create priorit_queue */
+        priority_queue<pair<int,int>>pq; 
+        for(auto&x : hash_map)
+        {
+           pq.emplace(make_pair(x.second,x.first));
+        }
+        /*will sort according to x.second*/
+       for(int i=0;i<k;i++)
+       {
+           auto it = pq.top();
+           kmostfrequent_vector.push_back(it.second);
+           pq.pop();
+       }
+        reverse(kmostfrequent_vector.begin(),kmostfrequent_vector.end());
+        return kmostfrequent_vector;
+        
+    }
+};

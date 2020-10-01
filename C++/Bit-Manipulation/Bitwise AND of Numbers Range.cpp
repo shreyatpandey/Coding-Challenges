bd76a968102adf0dc4df2d8985b8acf8538625f6
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2020-10-01 13:30:31 -0700

    Bitwise AND of Numbers Range

---
+++ C++/Bit-Manipulation/Bitwise AND of Numbers Range.cpp
@@ -0,0 +1,21 @@
+/*
+Given a range [m, n] where 0 <= m <= n <= 2147483647, return the bitwise AND of all numbers in this range, inclusive.
+
+Example 1:
+
+Input: [5,7]
+Output: 4
+
+*/
+class Solution {
+public:
+    int rangeBitwiseAnd(int m, int n) {
+        while ( m < n )
+        {
+            //turn off rightmost 1-bit
+            n = n & (n-1);
+        }
+        return n;
+        
+    }
+};

fa5a663d1fd8a0dfbf9e02785796cea28f7846fa
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2020-10-08 12:40:38 -0700

    Random Pick with Weight

---
+++ C++/Random-Sampling/Random Pick With Weight.cpp
@@ -0,0 +1,50 @@
+/*
+You are given an array of positive integers w where w[i] describes the weight of ith index (0-indexed).
+
+We need to call the function pickIndex() which randomly returns an integer in the range [0, w.length - 1]. 
+pickIndex() should return the integer proportional to its weight in the w array. For example, for w = [1, 3], the probability of picking the index 0 is 1 / (1 + 3) = 0.25 (i.e 25%) while the probability of picking the index 1 is 3 / (1 + 3) = 0.75 (i.e 75%).
+
+More formally, the probability of picking index i is w[i] / sum(w).
+
+ Input
+["Solution","pickIndex","pickIndex","pickIndex","pickIndex","pickIndex"]
+[[[1,3]],[],[],[],[],[]]
+Output
+[null,1,1,1,1,0]
+
+Explanation
+Solution solution = new Solution([1, 3]);
+solution.pickIndex(); // return 1. It's returning the second element (index = 1) that has probability of 3/4.
+solution.pickIndex(); // return 1
+solution.pickIndex(); // return 1
+solution.pickIndex(); // return 1
+solution.pickIndex(); // return 0. It's returning the first element (index = 0) that has probability of 1/4.
+*/
+class Solution {
+    private:
+        vector<int>SumOfWeights;
+public:
+    Solution(vector<int>& nums) {
+         this->SumOfWeights = nums;
+    }
+    
+    int pick(int target) {
+       int count = 0, res = -1;
+      for (int i = 0; i <SumOfWeights.size(); ++i)
+      {
+          if(SumOfWeights[i] == target && (!(rand()%++count))) 
+          {
+              std::cout<<"rand()%count:"<<rand()%count<<std::endl;
+              res=i;
+          }
+      }
+    return res;    
+        
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(nums);
+ * int param_1 = obj->pick(target);
+ */

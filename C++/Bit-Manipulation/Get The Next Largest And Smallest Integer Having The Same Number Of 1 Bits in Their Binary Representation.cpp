0c8e103ef6496ad83be8bae4c120b4ac202e0e03
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2020-10-11 20:42:48 -0700

    Next Largest And Smallest Integer With Same Number Of 1 Bits

---
+++ C++/Bit-Manipulation/Get The Next Largest And Smallest Integer Having The Same Number Of 1 Bits in Their Binary Representation.cpp
@@ -0,0 +1,34 @@
+//CTCI
+class Solution
+{
+  public:
+    int NextLargest(int Input)
+    {
+      int Value = Input;
+      int NumberOfZeros = 0;
+      int NumberOfOnes = 0;
+      while ( (Value & 1)==0 && ( Value != 0))
+      {
+        NumberOfZeros += 1;
+        Value >>= 1;
+      }
+      while( (Value&1)==1)
+      {
+        NumberOfOnes += 1;
+        Value >>= 1;
+      }
+      /*Error case for Integer Overflow */
+      if ( NumberOfZeros + NumberOfOnes == 31 || NumberOfZeros + NumberOfOnes == 1)
+      {
+        return -1;
+      }
+      int p = NumberOfOnes + NumberOfZeros; //position of rightmost non-trailing zero
+      Input |= ( 1<<p); // Flip rightmost non-trailing zero
+      Input &= ~((1<<p) - 1 ); //Clear all the bits to the right of p
+      Input |= (1<<(NumberOfOnes-1))-1; //Insert (NumberOfOnes-1) ones on the right
+      return Input;
+      
+     }
+      
+  };
+    

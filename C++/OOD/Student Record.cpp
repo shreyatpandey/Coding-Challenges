8f05dd274ec11d6b74441a7b59a0f17a10d2bc86
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2020-10-13 14:57:03 -0700

    Update Student Record.cpp

--- C++/OOD/Student Record.cpp
+++ C++/OOD/Student Record.cpp
@@ -112,3 +112,110 @@ int main()
     
 }
 
+#include<iostream>
+#include<string>
+#include<unordered_map>
+#include<vector>
+using namespace std;
+
+#define MAX_STUDENT 2
+
+/* Approach-2 */
+/* Using vector or array of objects*/
+class Student
+{
+    private:
+        string StudentName ;
+        vector<int>Grades ;
+        double Average ;
+    public:
+        Student(string _student_name)
+        {
+            this->StudentName = _student_name;
+            Average = 0;
+        }
+        string GetName ()
+        {
+            return this->StudentName ;
+        }
+        void AddGrades (vector<int>&grades) /*clarify if only the grades as given as input or courses as well */
+        {
+            this->Grades = grades;
+        }
+        vector<int>GetGrades()
+        {
+            return (this->Grades);
+        }
+        double AverageOfGrades()
+        {
+            int sum = 0;
+            for(int num:Grades)
+            {
+                sum += num;
+            }
+            this->Average = (double)sum/Grades.size();
+            return (this->Average);
+        }
+} ;
+int main()
+{
+    string HoldStudentName;
+    /* Approach-1 Input Student Name */
+    /*
+    cout<<"---Input Student Name---"<<endl;
+    cin>>HoldStudentName;
+    */
+    
+    vector<Student>Objects;
+    for(int i=0;i<MAX_STUDENT;i++)
+    {
+        cout<<"Enter Student Name:"<<endl;
+        cin>>HoldStudentName;
+        Objects.emplace_back(Student(HoldStudentName));
+        cout<<"Enter Marks:"<<endl;
+        vector<int>Marks;
+        int value;
+        for(int j=0;j<3;j++)
+        {
+            cin>>value;
+            Marks.emplace_back(value);
+        }
+        Objects[i].AddGrades(Marks);
+        
+    }
+    /*Display Content*/
+    cout<<"Student    |"<<"Grade"<<endl;
+    for(int i=0;i<MAX_STUDENT;i++)
+    {
+        cout<<Objects[i].GetName()<<"   ";
+        auto GradeResult = Objects[i].GetGrades();
+        for(int num:GradeResult)
+        {
+            cout<<num<<" ";
+        }
+        cout<<endl;
+    }
+    /*Search Content*/
+    cout<<"Enter Student Name:"<<endl;
+    cin>>HoldStudentName;
+    int i;
+    for(i=0;i<MAX_STUDENT;i++)
+    {
+        if ( Objects[i].GetName() == HoldStudentName)
+        {
+            cout<<"Cumulative Grade:"<<Objects[i].AverageOfGrades()<<endl;
+            exit(0);
+        }
+    }
+    if ( i == MAX_STUDENT-1)
+    {
+        cout<<"No Record Found"<<endl;
+        exit(0);
+    }
+    
+    
+    return 0;
+    
+}
+
+

5f4d0863213b8ae3a4db1daaabe678e8855c40a2
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2021-01-17 14:00:44 -0800

    ReverseOnlyLetters

---
+++ C++/string/Reverse Only Letters.cpp
@@ -0,0 +1,35 @@
+/*
+Given a string S, return the "reversed" string where all characters that are not a letter stay in the same place, and all letters reverse their positions.
+Example 1:
+
+Input: "ab-cd"
+Output: "dc-ba"
+*/
+/* 
+No Edge Case check like string is null
+Any tabluar spaces or new line character
+*/
+class Solution {
+public:
+    string reverseOnlyLetters(string S) {
+        stack<char>HoldChar;
+        int j  = 0;
+        while ( j < S.length())
+        {
+            if ( isalpha(S[j]))
+                HoldChar.push(S[j]);
+            j +=1 ;
+        }
+        j = 0;
+        while(j<S.length())
+        {
+            if ( isalpha(S[j]) )
+            {
+                S[j] = HoldChar.top();
+                HoldChar.pop();
+            }
+            j += 1;
+        }
+        return S;
+    }
+};

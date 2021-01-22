f2a0d265d90835a78e9706aac0ef2791a9f8afc7
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2021-01-21 19:53:24 -0800

    SquareSortedArrayInDifferentWay

---
+++ C++/GenericProgramming|Template/SquareSortedArrayInDifferentWay.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+using Array = std::vector<int> ;
+
+int main()
+{
+    Array Input = {-4,-3,-2,0,1,4,9};
+    auto Temp = Input.size() -1 ;
+    auto StartIndex = 0;
+    auto EndIndex = Input.size() -1;
+    Array SquareSortedArray(Input.size(),0);
+    while(StartIndex <= EndIndex)
+    {
+        if ( Input[StartIndex]*Input[StartIndex] <= Input[EndIndex]*Input[EndIndex] )
+        {
+            SquareSortedArray[Temp--] = Input[EndIndex]*Input[EndIndex];
+            EndIndex -= 1;
+        }
+        else
+        {
+          SquareSortedArray[Temp--] = Input[StartIndex]*Input[StartIndex] ;
+          StartIndex += 1 ;
+        }
+    }
+    for(auto &x:SquareSortedArray)
+    {
+        cout<<x<<" ";
+    }
+    cout<<endl;
+    return 0;
+    
+}

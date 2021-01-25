17f2e6794dd0709b06d937a4278398bbc70d821c
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2021-01-24 18:16:39 -0800

    Missing Element in Sorted Array

--- C++/Binary Search/Missing Element in Sorted Array.cpp
+++ C++/Binary Search/Missing Element in Sorted Array.cpp
@@ -7,29 +7,25 @@ Output: 5
 Explanation: 
 The first missing number is 5.
 */
+
 class Solution {
-    private:
-        int missingnumber(int indexvalue,vector<int>&nums)
-        {
-            return ( nums[indexvalue] - nums[0] - indexvalue);
-        }
 public:
     int missingElement(vector<int>& nums, int k) {
-        int numslength = nums.size();
-        
-        if( k > missingnumber (numslength-1,nums))
-            return nums[numslength-1] + k - missingnumber(numslength-1,nums) ;
-        
-        int low = 0 , high = numslength-1 , mid = 0;
-        while ( low != high)
+    int LeftIndex = 0, RightIndex = nums.size();
+    while(LeftIndex < RightIndex)
+    {
+        int MidValue = (LeftIndex + RightIndex)>>1 ;
+        int Difference = (nums[MidValue] - MidValue) - nums[0] ;
+        if ( Difference < k )
         {
-            mid = low + (high-low)/2;
-            if ( missingnumber(mid,nums)<k)
-                low = mid+1;
-            else
-                high = mid;
+            LeftIndex = MidValue + 1;
         }
-        return ( nums[low-1] + k - missingnumber(low-1,nums));
+        else
+        {
+            RightIndex = MidValue ;
+        }
+    }
+        return RightIndex + nums[0] + k -1 ;
         
     }
 };

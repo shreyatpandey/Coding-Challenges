ea6899e4a91fc63d3ec8d94856baa2f725a0e749
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2021-01-26 16:56:16 -0800

    LRU Cache

--- C++/data_structures/LRU Cache.cpp
+++ C++/data_structures/LRU Cache.cpp
@@ -2,50 +2,57 @@
 //Approach 
 //TC: O(1)
 //Space Complexity: O(m+n) where m is the space for hashmap and n is the space for list
+using LRU = std::unordered_map<int,list<pair<int,int>>::iterator>; // constant operation:- O(1)
+using Cache = std::list<pair<int,int>>; //doubly linked list
 class LRUCache {
 public:
     const int capacity;
-    list<pair<int,int>> l; // This is cache
-    unordered_map<int, list<pair<int,int > >::iterator> m; // This is used to make 
-    // Least Recently = Map + Cache = List
+    Cache cache; // This is Cache
+    LRU lru;// This is LRU
     
     int remove(list<pair<int,int > >::iterator it) 
     {
         int key = it->first;
         int val = it->second ;
-        l.erase(it);
-        m.erase(key);
+        cache.erase(it);
+        lru.erase(key);
         return (val);
     }
     
     void add(const int & key, const int & value) 
     {
-        l.push_back({key, value});
-        auto it = l.end();
-        m[key] = --(l.end());
+        auto  KeyValuePair = make_pair(key,value);
+        cache.push_back(KeyValuePair);
+        auto it = cache.end();
+        lru[key] = --(it);
     }
     
     LRUCache(int capacity) : capacity(capacity) {}
+    ~LRUCache()
+    {
+        lru.clear();
+        cache.erase(cache.begin(),cache.end());
+    }
     
     int get(int key) 
     {
-        if(!m.count(key)) 
+        if(lru.count(key) == 0 ) 
         {
             return -1;
         }
-        int value = remove(m[key]);
+        int value = remove(lru[key]);
         add(key, value);
         return value;
     }
     
     void put(int key, int value) 
     {
-        if(m.count(key))
+        if(lru.count(key))
         {
-            remove(m[key]);
+            remove(lru[key]);
         }
-        if(l.size() == capacity) 
-            remove(l.begin());
+        if(cache.size() == capacity) 
+            remove(cache.begin());
         
         add(key, value);
     }

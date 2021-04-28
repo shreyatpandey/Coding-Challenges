eb56ebc6eaa361f5051da22223e4030924f6a49d
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2021-04-27 19:28:36 -0700

    Add String.cpp

--- C++/string/Add String.cpp
+++ C++/string/Add String.cpp
@@ -1,5 +1,46 @@
 /*This program is another way to implement atoi*/
 
+//78.54% faster
+//TC:- O(max(n1,n2))
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        int i = 0;
+        reverse(num1.begin(),num1.end()) ;
+        reverse(num2.begin(),num2.end()) ;
+        int MaxOfTwoLength = max(num1.size(),num2.size());
+        string AddStringResult = "";
+        int carry = 0;
+        while ( i <= MaxOfTwoLength-1 )
+        {
+            int digit1 = 0 , digit2 = 0 ;
+            if ( i <= num1.size()-1) 
+            {
+                digit1 = num1[i] - (int)'0' ;
+            }
+            if ( i<=num2.size() - 1) 
+            {
+                digit2 = num2[i] - (int)'0' ;
+            }
+            int DigitResult = digit1 + digit2 + carry ;
+            int Remainder = DigitResult % 10 ;
+            AddStringResult += (Remainder + (int)'0') ;
+            carry = DigitResult/10 ;
+            i += 1;
+        }
+        if ( carry ) AddStringResult +='1';
+        reverse(AddStringResult.begin(),AddStringResult.end());
+        return AddStringResult ; 
+    }
+};
+
+
+
+
+
+
+
+/* Will not work for INT_MAX , int overflow */
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -36,17 +77,7 @@ class Solution
  return result;
  }
 };
-int main()
-{
-   string s1="105";
-   string s2="420";
-    
-   Solution s;
-   string final_result = s.addStrings(s1,s2);
-   cout<<"Final_result_string:"<<final_result<<endl;
-   return 0;
 
-}
    
    
 

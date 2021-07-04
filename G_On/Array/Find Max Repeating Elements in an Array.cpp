aaa85d8779a2243d7615e25f8b8da579d83e84f2
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2021-07-04 11:40:37 -0700

    Find Max Repeating Elements in an Array

---
+++ G_On/Array/Find Max Repeating Elements in an Array.cpp
@@ -0,0 +1,25 @@
+//Input = [1, 2, 2, 2, 0, 2, 0, 2, 3, 8, 0, 9, 2, 3]
+//Output:- 2
+//Input = {4, 1, 7, 2, 7, 5, 9, 8, 6, 1, 7, 2, 4, 7}
+//Output:- 7
+int MaxRepeatingElements(vector<int>&Input)
+{
+ int Size = Input.size();
+ int MaxCount = 0;
+ int MaxIndex = 0;
+ for(int i=0;i<Size;i++)
+ {
+  int Index = Input[i]%Size;
+   Input[Index] = Input[Index] + Size;
+ }
+ for (int i = 0; i <Size ; i++) 
+ {
+         if(Input[i]/size>MaxCount)
+            {
+                MaxCount = Input[i]/Size;
+                MaxIndex = i;
+            }
+    }
+ return MaxCount ; //quesiton can be twisted to return MaxIndex or MaxCount 
+  
+}

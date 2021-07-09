ec21b0dfc0d505aed58563112dc22c8516344986
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2021-07-08 20:39:47 -0700

    Course Schedule-I

---
+++ C++/Graphs/Kahn's Algo-Topo/Course Schedule -I.cpp
@@ -0,0 +1,45 @@
+/*
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. 
+You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+
+For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+Return true if you can finish all courses. Otherwise, return false.
+ 
+Example 1:
+
+Input: numCourses = 2, prerequisites = [[1,0]]
+Output: true
+Explanation: There are a total of 2 courses to take. 
+To take course 1 you should have finished course 0. So it is possible.
+
+*/
+
+
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        unordered_map<int, unordered_set<int>> in_degree, out_degree;
+        for (const auto& p : prerequisites) {
+            in_degree[p[0]].emplace(p[1]);
+            out_degree[p[1]].emplace(p[0]);
+        }
+        vector<int> stk;
+        for (int i = 0; i < numCourses; ++i) {
+            if (!in_degree.count(i)) {
+                stk.emplace_back(i);
+            }
+        }
+        while (!stk.empty()) {
+            const auto node = stk.back(); stk.pop_back();
+            for (const auto& i : out_degree[node]) {
+                in_degree[i].erase(node);
+                if (in_degree[i].empty()) {
+                    stk.emplace_back(i);
+                    in_degree.erase(i);
+                }
+            }
+            out_degree.erase(node);
+        }
+        return in_degree.empty() && out_degree.empty();
+    }
+};

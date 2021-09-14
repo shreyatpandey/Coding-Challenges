43dd27b825d782d5900c420651cae1bbbdd10053
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2021-09-13 22:40:23 -0700

    PythonSource.py

---
+++ MSFT-OA/Minimum Deletions to Make Character Frequencies Unique/PythonSource.py
@@ -0,0 +1,25 @@
+import heapq
+class Solution(object):
+    def minDeletions(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        freqMap = {}
+        maxheap = []
+        ans = 0
+        for char in s:
+            if char not in freqMap : freqMap[char] = 1
+            else : freqMap[char] += 1
+        for key,val in freqMap.items():
+            heapq.heappush(maxheap,-val)
+        
+        while len(maxheap)>0:
+            currentFreq = heapq.heappop(maxheap)
+            if len(maxheap) == 0: return ans
+            if currentFreq  == maxheap[0]:
+                if currentFreq + 1 < 0 :
+                    heapq.heappush(maxheap,currentFreq+1)
+                ans += 1
+        return ans
+        return 0

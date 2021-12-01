e13977884e33e6a662ecea2bf7e9db3b7893dc2a
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2021-12-01 15:35:59 -0800

    Update Divide Two Integers.cpp

--- C++/Bit-Manipulation/Divide Two Integers.cpp
+++ C++/Bit-Manipulation/Divide Two Integers.cpp
@@ -1,108 +1,31 @@
-//Approach-1
-//Leetcode Solution
+//TC:- O(N) , N = 32
+//SC:- O(1)
 class Solution {
-    private:
-        int HALF_INT_MIN = INT_MIN/2;
 public:
     int divide(int dividend, int divisor) {
-        if(!(divisor)||dividend == INT_MIN && divisor == -1)
-		return INT_MAX;
-        int negatives = 2;
-    if (dividend > 0) {
-        negatives--;
-        dividend = -dividend;
-    }
-    if (divisor > 0) {
-        negatives--;
-        divisor = -divisor;
-    }
-
-    int quotient = 0;
-    /* Once the divisor is bigger than the current dividend,
-     * we can't fit any more copies of the divisor into it. */
-    while (divisor >= dividend) {
-        /* We know it'll fit at least once as divivend >= divisor.
-         * Note: We use a negative powerOfTwo as it's possible we might have
-         * the case divide(INT_MIN, -1). */
-        int powerOfTwo = -1;
-        int value = divisor;
-        /* Check if double the current value is too big. If not, continue doubling.
-        * If it is too big, stop doubling and continue with the next step */
-        while (value >= HALF_INT_MIN && value + value >= dividend) {
-            value += value;
-            powerOfTwo += powerOfTwo;
-        }
-        // We have been able to subtract divisor another powerOfTwo times.
-        quotient += powerOfTwo;
-        // Remove value so far so that we can continue the process with remainder.
-        dividend -= value;
-    }
-
-    /* If there was originally one negative sign, then
-     * the quotient remains negative. Otherwise, switch
-     * it to positive. */
-    if (negatives != 1) {
-        quotient = -quotient;
-    }
-    return quotient;
-}
-       
-};
-
-//Approach-2
-//CTCI:- Does not work for the case INT_MAX and divisor = 2 or so on
-class Solution {
-    private:
-        int negate(int a);
-public:
-    int divide(int dividend, int divisor) {
-        //handling overflow cases for INT_MAX 
-        if ( (dividend == INT_MIN && divisor == -1) || (dividend == INT_MAX && divisor == 1) )
-            return INT_MAX;
-        //handling overflow cases for INT_MIN
-        if ((dividend == INT_MIN && divisor == 1  || (dividend == INT_MAX && divisor == -1)))
-            return INT_MIN;
+        int sign = (dividend<0)^(divisor<0)?-1:1;
+        long long dvd = dividend;
+        long long dvs = divisor;
         
-        int absa = abs(dividend);
-        int absb = abs(divisor);
-        
-        int product  = 0;
-        int x = 0;
-        while ( (product + absb <= absa) ) //Integer overflow  
-        {
-            product += absb ;
-            x++;
-        }
-        if ( (dividend<0 && divisor<0) || (dividend>0 && divisor>0))
-        {
-            return x;
-        }
-        else
-        {
-            return negate(x);
+        if (dvd == INT32_MIN && dvs == -1) {
+            return INT32_MAX;
         }
         
+        dvd = abs(dvd);
+        dvs = abs(dvs);
+        
+        long long res = 0;
+        
+        for (int i = 31; i >= 0; --i) {
+          if (dvd >= (dvs << i)) {
+            dvd -= (dvs << i);
+            res += (1 << i);
+          }
+        }
+
+        return sign * res;
+    
     }
 };
-int Solution::negate(int a)
-{
-    int neg = 0;
-    int newSign = a < 0? 1:-1;
-    int delta = newSign ;
-    while ( a != 0)
-    {
-        bool differentSigns = ( a + delta > 0) != (a > 0);
-        if ( a + delta !=0 && differentSigns)
-        {
-            delta = newSign;
-        }
-        neg += delta ;
-        a += delta ;
-        delta += delta;
-    }
-    return neg;
-}
-
-
 	
 	

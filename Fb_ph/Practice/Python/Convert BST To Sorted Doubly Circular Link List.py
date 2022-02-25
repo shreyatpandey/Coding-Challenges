3994be6a3ef1cdf48bc90f0fc5aef16084e28b56
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-02-24 21:40:54 -0800

    Create Convert BST To Sorted Doubly Circular Link List.py

---
+++ Fb_ph/Practice/Python/Convert BST To Sorted Doubly Circular Link List.py
@@ -0,0 +1,32 @@
+'''
+Python InOrder Traversal
+Time-Complexoty:- O(n)
+Just use inorder traversal, which finds the nodes in ascending order, and store the head and previous node in global variables. 
+After the traversal is finished, prev is the "tail" of the double linked list so just connect it to the head.
+Time-Complexity:- O(N)
+Space-Complexioty:- O(N) for the recursion stack
+
+Convert a Binary Search Tree to a sorted Circular Doubly-Linked List in place.
+'''
+
+class Solution(object):
+    head = None
+    prev = None
+
+    def treeToDoublyList(self, root):
+        if not root: return None
+        self.treeToDoublyListHelper(root)
+        self.prev.right = self.head
+        self.head.left = self.prev
+        return self.head
+
+    def treeToDoublyListHelper(self, node):
+        if not node: return
+        self.treeToDoublyListHelper(node.left)
+        if self.prev:
+            node.left = self.prev
+            self.prev.right = node
+        else:  # We are at the head.
+            self.head = node
+        self.prev = node
+        self.treeToDoublyListHelper(node.right)

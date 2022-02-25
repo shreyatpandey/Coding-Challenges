f85f3c762c480314b1430692d7e5660a65add628
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-02-24 21:14:23 -0800

    Update Random Pick Weight.py

--- Fb_ph/Practice/Python/Random Pick Weight.py
+++ Fb_ph/Practice/Python/Random Pick Weight.py
@@ -44,3 +44,35 @@ Pseudocode
 4. put weights on the number line
 5. If a uniform varialbe falls in the range of a value that value is returned
 '''
+
+'''
+Time-Complexity:- 
+For init - O(N)
+For pickindex - O(Log(N))
+'''
+class Solution:
+    def __init__(self, w: List[int]):
+        """
+        :type w: List[int]
+        """
+        self.prefix_sums = []
+        prefix_sum = 0
+        for weight in w:
+            prefix_sum += weight
+            self.prefix_sums.append(prefix_sum)
+        self.total_sum = prefix_sum
+
+    def pickIndex(self) -> int:
+        """
+        :rtype: int
+        """
+        target = self.total_sum * random.random()
+        # run a binary search to find the target zone
+        low, high = 0, len(self.prefix_sums)
+        while low < high:
+            mid = low + (high - low) // 2
+            if target > self.prefix_sums[mid]:
+                low = mid + 1
+            else:
+                high = mid
+        return low

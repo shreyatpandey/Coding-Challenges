5b3c375e3bbb26a1e337fbb82f52694770ac2af4
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-02-28 08:25:35 -0800

    Update Simplify Path.py

--- Fb_ph/Practice/Python/Simplify Path.py
+++ Fb_ph/Practice/Python/Simplify Path.py
@@ -13,3 +13,37 @@ class Solution:
             else:
                 stack.append(token)
         return '/' + '/'.join(stack)
+ 
+
+'''
+Solution Beats 99.62%
+'''
+class Solution(object):
+    def simplifyPath(self, path):
+        """
+        :type path: str
+        :rtype: str
+        """
+        result = "/"
+        stk = []
+        listpath = path.split("/")
+        for char in listpath:
+            if char == "." or char =='':
+                continue
+        
+            elif char == ".." :
+                if len(stk) > 0:
+                    del stk[-1]    
+        
+            else:
+                stk.append(char)
+        
+        while len(stk) > 0:
+            lastchar = stk[0]
+            del stk[0]
+            if len(stk) == 0:
+                result += lastchar 
+            else:
+                result += lastchar + "/"
+    
+        return result

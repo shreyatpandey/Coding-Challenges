f418e3f0284c3fe82ccb10dbfe5cdae7cfe3bfc5
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-02-28 21:51:29 -0800

    Update Valid Palindrome II.py

--- Fb_ph/Practice/Python/Valid Palindrome II.py
+++ Fb_ph/Practice/Python/Valid Palindrome II.py
@@ -26,3 +26,23 @@ def validPalindrone(s):
     return True
 
   return isPalindrome(0, len(s) - 1, s, 0)
+
+'''
+TC:- O(len(inputString))
+SC:- O(n)
+'''
+
+def validPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        # Time: O(n)
+        # Space: O(n)
+        left, right = 0, len(s) - 1
+        while left < right:
+            if s[left] != s[right]:
+                one, two = s[left:right], s[left + 1:right + 1]
+                return one == one[::-1] or two == two[::-1]
+            left, right = left + 1, right - 1
+        return True

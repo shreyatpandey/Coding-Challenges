92b0955ef553f8747d706f0d67da402fd9b0138d
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-02-28 21:26:56 -0800

    Pow(x,n)

---
+++ Fb_ph/Practice/Python/Pow(x,n).py
@@ -0,0 +1,17 @@
+'''
+TC:- O(Log(N))
+SC:- O(1)
+'''
+'''
+Implement pow(x,n) which calculates x raised to the power n (i.e., xn)
+'''
+class Solution:
+    def myPow(self, x, n):
+        m = abs(n)
+        ans = 1.0
+        while m:
+            if m & 1:
+                ans *= x
+            x *= x
+            m >>= 1
+        return ans if n >= 0 else 1 / ans

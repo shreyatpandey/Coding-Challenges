144fd807e9a94b8fdf8d65dc1ddba8046c782aa8
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-03-02 22:02:35 -0800

    Create Add to Array.py

---
+++ Fb_ph/Practice/Python/Add to Array.py
@@ -0,0 +1,25 @@
+'''
+The array-form of an integer num is an array representing its digits in left to right order.
+For example, for num = 1321, the array form is [1,3,2,1].
+Given num, the array-form of an integer, and an integer k, return the array-form of the integer num + k.
+Example 1:
+
+Input: num = [1,2,0,0], k = 34
+Output: [1,2,3,4]
+Explanation: 1200 + 34 = 1234
+'''
+
+'''
+TC:- O(max(N,logK))
+SC:- O(max(N.logK))
+'''
+class Solution:
+    def addToArrayForm(self, A: List[int], K: int) -> List[int]:
+        for i in range(len(A) - 1, -1, -1):
+            if not K: 
+                break
+            K, A[i] = divmod(A[i] + K, 10)
+        while K:
+            K, a = divmod(K, 10)
+            A = [a] + A
+        return A

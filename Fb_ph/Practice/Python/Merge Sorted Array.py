c5e2049025e3a2be6b34bb9f6ae45f17748eb846
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-03-07 21:07:05 -0800

    Merge Sorted Array

---
+++ Fb_ph/Practice/Python/Merge Sorted Array.py
@@ -0,0 +1,53 @@
+'''
+Approach-1:- Three Pointers
+TC:-O(n+m)
+SC:- O(m)
+'''
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        # Make a copy of the first m elements of nums1.
+        nums1_copy = nums1[:m] 
+        
+        # Read pointers for nums1Copy and nums2 respectively.
+        p1 = 0
+        p2 = 0
+        
+        # Compare elements from nums1Copy and nums2 and write the smallest to nums1.
+        for p in range(n + m):
+            # We also need to ensure that p1 and p2 aren't over the boundaries
+            # of their respective arrays.
+            if p2 >= n or (p1 < m and nums1_copy[p1] <= nums2[p2]):
+                nums1[p] = nums1_copy[p1] 
+                p1 += 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 += 1
+ '''
+ Approach-2:- Three Pointers
+ TC:- O(n+m)
+ SC:- O(1)
+ '''
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        
+        # Set p1 and p2 to point to the end of their respective arrays.
+        p1 = m - 1
+        p2 = n - 1
+    
+        # And move p backwards through the array, each time writing
+        # the smallest value pointed at by p1 or p2.
+        for p in range(n + m - 1, -1, -1):
+            if p2 < 0:
+                break
+            if p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1

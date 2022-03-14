6619cba690df880b8e35ad2368698ffe61f4c386
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-03-13 21:37:04 -0700

    Create Next Greater Element III.py

---
+++ Fb_ph/Practice/Python/Next Greater Element III.py
@@ -0,0 +1,23 @@
+'''
+TC:- O(len(str))
+SC:- O(1)
+'''
+class Solution:
+    def nextGreaterElement(self, n: int) -> int:
+        n = [x for x in str(n)]
+        size = len(n)
+        ans = -1
+        for i in range(size-1,-1,-1):
+            if n[i-1] < n[i] and i>0:
+                if i == size-1 or n[-1]==n[i]: 
+                    n[i-1], n[i]=n[i],n[i-1]
+                    ans = int(''.join(n))
+                    return -1 if ans > 2147483647 else ans
+                else:
+                    for j in range(size-1, i-1, -1):
+                        if n[j]>n[i-1]:
+                            n[i-1], n[j]=n[j],n[i-1]
+                            break 
+                    ans = int(''.join(n[:i]+n[i:][::-1]))
+                return -1 if ans > 2147483647 else ans
+        return ans

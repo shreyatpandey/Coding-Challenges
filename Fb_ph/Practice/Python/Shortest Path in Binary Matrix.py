24ab76f91229fe3415c5381cab2da17b58fdc150
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-03-15 08:34:47 -0700

    Update Shortest Path in Binary Matrix.py

--- Fb_ph/Practice/Python/Shortest Path in Binary Matrix.py
+++ Fb_ph/Practice/Python/Shortest Path in Binary Matrix.py
@@ -18,13 +18,16 @@ Comment:- To avoid revisiting we can securely set the visiting grid as non-empty
 
 def shortestPathBinaryMatrix(grid):
 	n = len(grid)
+	dirs = [(-1,-1),(-1,0),(0,-1),(1,0),(1,-1),(0,1),(-1,1),(1,1)]
 	if grid[0][0] or grid[n-1][n-1]:
 		return -1
 	q = [(0, 0, 1)]
 	grid[0][0] = 1
 	for i, j, d in q:
 		if i == n-1 and j == n-1: return d
-		for x, y in ((i-1,j-1),(i-1,j),(i-1,j+1),(i,j-1),(i,j+1),(i+1,j-1),(i+1,j),(i+1,j+1)):
+		for row,col in dirs:
+			x = row + i
+			y = col + j
 			if 0 <= x < n and 0 <= y < n and not grid[x][y]:
 				grid[x][y] = 1
 				q.append((x, y, d+1))

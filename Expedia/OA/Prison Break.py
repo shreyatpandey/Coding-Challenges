e2e05e0bb0d1abb22ae41001c5ffb321197c3649
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-03-15 22:41:00 -0700

    Update Prison Break.py

--- Expedia/OA/Prison Break.py
+++ Expedia/OA/Prison Break.py
@@ -12,24 +12,24 @@ the values in array h are distinct
 the values in array v are distinct
 '''
 def prisont(n,m,h,v):
-  _x = [0*i for i in range(n+1)]
-  _y = [0*i for i in range(m+1)]
-  for hor in h:
-    _x[hor] = 1
-  for vert in v:
-    _y[vert] = 1
-  x_,y_ = 0,0
-  j = 0
-  for i in range(1,n+1):
-    if not _x[i]:
-      j = 0
-    else:
-      j += 1
-      x_ = max(x_,j)
-   for i in range(1,m+1):
-    if not _y[i]:
-      j = 0
-    else:
-      j += 1
-      y_ = max(y_,j)
-   return (x_+1)*(y_+1)
+    _x = [0*i for i in range(n+1)]
+    _y = [0*i for i in range(m+1)]
+    for hor in h:
+        _x[hor] = 1
+    for vert in v:
+        _y[vert] = 1
+    x_,y_ = 0,0
+    j = 0
+    for i in range(1,n+1):
+        if not _x[i]:
+            j = 0
+        else:
+            j += 1
+            x_ = max(x_,j)
+    for i in range(1,m+1):
+        if not _y[i]:
+            j = 0
+        else:
+            j += 1
+            y_ = max(y_,j)
+    return (x_+1)*(y_+1)

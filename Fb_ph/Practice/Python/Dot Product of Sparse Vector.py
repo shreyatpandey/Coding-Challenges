785963ed6d647168481bb8a1f16fb9e0f67e25b1
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-03-19 15:14:00 -0700

    Update Dot Product of Sparse Vector.py

--- Fb_ph/Practice/Python/Dot Product of Sparse Vector.py
+++ Fb_ph/Practice/Python/Dot Product of Sparse Vector.py
@@ -1,5 +1,6 @@
 '''
 TC:- O(Len(Dictionary))
+SC:- O(Dictionary)
 '''
 class SparseVector:
     def __init__(self, nums: List[int]):
@@ -17,3 +18,19 @@ class SparseVector:
                     result += self.nums[key] * vec.nums[key]
                     
         return result
+
+    '''
+    Without HashMap
+    FB interview accept this solution
+    TC:- O(n) for the result of calculating dot product or vector
+    SC:- O(1) 
+    '''
+    class SparseVector:
+        def __init__(self, nums):
+            self.array = nums
+
+        def dotProduct(self, vec):
+            result = 0
+            for num1, num2 in zip(self.array, vec.array):
+                result += num1 * num2
+            return result

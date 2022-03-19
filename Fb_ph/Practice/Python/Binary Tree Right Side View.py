ae88da6e1c3957031e5fa5b4d682a4cc6afb05b9
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-03-19 16:44:39 -0700

    Create Binary Tree Right Side View.py

---
+++ Fb_ph/Practice/Python/Binary Tree Right Side View.py
@@ -0,0 +1,38 @@
+#Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+'''
+Approach:- BFS
+TC:- O(N) where N is the number of nodes
+SC:- O(N) is the space of Queue/Deque
+'''
+
+from collections import deque
+class Solution(object):
+    def rightSideView(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        queue = []
+        BinaryTreeRightSideView = []
+        if not root:
+            return BinaryTreeRightSideView
+        queue.append(root)
+        while len(queue) != 0:
+            LenQueue = len(queue)
+            for i in range(LenQueue):
+                node = queue[0]
+                del queue[0]
+                if i == LenQueue -1 :
+                    BinaryTreeRightSideView.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        return BinaryTreeRightSideView
+        

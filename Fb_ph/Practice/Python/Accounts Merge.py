6c5c909532160d241c4e090da76fc016fa119597
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-03-20 19:45:59 -0700

    Update Accounts Merge.py

--- Fb_ph/Practice/Python/Accounts Merge.py
+++ Fb_ph/Practice/Python/Accounts Merge.py
@@ -142,54 +142,43 @@ Stack:- O(NK)
 class Solution:
     def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
         
-        em_to_name = {}
-        em_graph = defaultdict(set)
+        emGraph = defaultdict(set)
+        emName = {}
+        for email in accounts:
+            name = email[0]
+            for emails in email[1:]:
+                emGraph[email[1]].add(emails)
+                emGraph[emails].add(email[1])
+                        
+                emName[emails] = name
+        #print("emGraph",emGraph)
+        #print("emName:",emName)
         
-        for acc in accounts:
-            name = acc[0]
-            
-            # making a graph of common connected gmail
-            # all acc the gamil start with 1 index
-            for email in acc[1:]:
-                
-                # connect 1st to 2nd email
-                em_graph[acc[1]].add(email)
-                
-                #connect 2nd to 1st email
-                em_graph[email].add(acc[1])
-                
-                # create a hashmap
-                # it help us to find the email owners
-                em_to_name[email] = name
-                
-        # print(em_graph)
-        # print(em_to_name)
-    
-        seen = set()
+        '''
+        Traverse the connected components
+        '''
         ans = []
+        visited = set()
         
-        # here we use loop to traverse all unconnected
-        # components of the graph
-        for email in em_graph:
-            if email not in seen:
-                seen.add(email)
-                 q = [email]
-                component = []
+        for email in emGraph:
+            if email not in visited:
+                visited.add(email)
+                q = []
+                q.append(email)
+                components = []
                 
-                # this loop give us the all conneted path as here
-                # all common gmail as a list in comonent
                 while q:
-                    edge = q.pop(0)
-                    component.append(edge)
-                    for nei in em_graph[edge]:
-                        if nei not in seen:
-                            seen.add(nei)
-                           q.append(nei)
-                            
-                # after geting all connect comonent
-                # we sorted the as question
-                # and search the owner of the starting email
-                # append in the ans
-                ans.append([em_to_name[email]] + sorted(component))
+                    edge = q.pop()
+                    components.append(edge)
+                    for neighbours in emGraph[edge]:
+                        if neighbours not in visited:
+                            q.append(neighbours)
+                            visited.add(neighbours)
+                
+                components.sort()
+                components.insert(0,emName[email])
+                ans.append(components)
+                #ans.append(components.insert(emName[email],0))
+        
         return ans
   

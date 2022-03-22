56c50218eef8f2c857a12a57c422f6572ea6dfff
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-03-21 21:30:48 -0700

    Create Shortest Distance From All Buildings.py

---
+++ Fb_ph/Practice/Python/Shortest Distance From All Buildings.py
@@ -0,0 +1,67 @@
+'''
+Questions:-
+You are given an m x n grid grid of values 0, 1, or 2, where:
+
+each 0 marks an empty land that you can pass by freely,
+each 1 marks a building that you cannot pass through, and
+each 2 marks an obstacle that you cannot pass through.
+You want to build a house on an empty land that reaches all buildings in the shortest total travel distance. 
+You can only move up, down, left, and right.
+
+Return the shortest travel distance for such a house. If it is not possible to build such a house according to the above rules, return -1.
+
+The total travel distance is the sum of the distances between the houses of the friends and the meeting point.
+
+The distance is calculated using Manhattan Distance, where distance(p1, p2) = |p2.x - p1.x| + |p2.y - p1.y|
+
+'''
+
+
+'''
+Algo:-
+Do a BFS from each building to find for each empty land its shortest distance to that particular building. 
+Then we iterate over all empty lands and find for each land its sum of shortest distances to all buildings, 
+and record the minimum value of such sums. Finally, we return the minimum value we recorded.
+'''
+'''
+Time complexity: O(k*l)
+Space complexity: O(m*n*k), 
+where m = len(grid), n = len(grid[0]) 
+k is the number of buildings in the grid, and l is the number of empty lands in the grid.
+'''
+class Solution(object):
+    def shortestDistance(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """        
+        def bfs(y0, x0):
+            seen = [[0]*n for _ in range(m)]
+            q = collections.deque([(y0, x0, 0)])
+            while q:
+                y, x, d = q.popleft()
+                for yp, xp in {(y-1,x), (y+1, x), (y, x+1), (y, x-1)}:
+                    if 0 <= yp < m and 0 <= xp < n and grid[yp][xp] == 0 and seen[yp][xp] == 0:
+                        seen[yp][xp] = 1
+                        if not rec[yp][xp]:
+                            rec[yp][xp] = [d+1]
+                        else:
+                            rec[yp][xp].append(d+1)
+                        q.append((yp, xp, d+1))
+                        
+        m = len(grid)
+        n = len(grid[0])
+        rec = [[None]*n for _ in range(m)]  
+        building_count = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    building_count += 1
+                    bfs(i, j)
+        minimum = float('inf')
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 0:
+                    if rec[i][j] and len(rec[i][j]) == building_count:
+                        minimum = min(minimum, sum(rec[i][j]))
+        return minimum if minimum != float('inf') else -1

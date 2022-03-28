67b79b7d77ad5f9a13c7eea6e6e003b4a0e6d2e6
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-03-27 19:36:42 -0700

    Random Pick Weight

--- Fb_ph/Practice/Python/Random Pick Weight.py
+++ Fb_ph/Practice/Python/Random Pick Weight.py
@@ -1,3 +1,31 @@
+'''
+ou are given a 0-indexed array of positive integers w where w[i] describes the weight of the ith index.
+
+You need to implement the function pickIndex(), 
+which randomly picks an index in the range [0, w.length - 1] (inclusive) and returns it. 
+The probability of picking an index i is w[i] / sum(w).
+
+For example, if w = [1, 3], the probability of picking index 0 is 1 / (1 + 3) = 0.25 (i.e., 25%), and 
+the probability of picking index 1 is 3 / (1 + 3) = 0.75 (i.e., 75%).
+ 
+
+Example 1:
+
+Input
+["Solution","pickIndex"]
+[[[1]],[]]
+Output
+[null,0]
+
+Explanation
+Solution solution = new Solution([1]);
+solution.pickIndex(); // return 0. The only option is to return 0 since there is only one element in w.
+'''
+
+
+
+
+
 # used for random number generation
 import random
 

2c54ce3680adb80ae050e28333c99f97bf5a3fa0
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-03-29 08:27:21 -0700

    Update Palindromic Substrings.py

--- Fb_ph/Practice/Python/Palindromic Substrings.py
+++ Fb_ph/Practice/Python/Palindromic Substrings.py
@@ -1,10 +1,17 @@
 '''
+Input = "bb"
+Output = 3
+
+Input = "aaa"
+Output = 6
+'''
+'''
 Approach:- Expansion around centers
 TC:- O(n^2)
 There are total of 2N-1 possible centers and each center can possibly expand to N which is the length of the string
 So, in total we have = N.(2N-1)  ~ N^2
 SC:- O(1)
-'''
+'''\
 class Solution:
     def countSubstrings(self, s: str) -> int:
         count = 0

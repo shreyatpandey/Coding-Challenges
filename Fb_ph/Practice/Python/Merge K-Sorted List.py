67d4ac8284dabcdb9a99bf136b57e9cef8ed6d41
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-03-29 08:54:54 -0700

    Update Merge K-Sorted List.py

--- Fb_ph/Practice/Python/Merge K-Sorted List.py
+++ Fb_ph/Practice/Python/Merge K-Sorted List.py
@@ -31,4 +31,39 @@ class Solution:
             p = p.next
         p.next = l or r
         return dummy.next
+'''
+Approach-2:- 
+TC:- O(NkLog(k))
+Sc:- O(1)
+'''
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        amount = len(lists)
+        interval = 1
+        while interval < amount:
+            for i in range(0, amount - interval, interval * 2):
+                lists[i] = self.merge2Lists(lists[i], lists[i + interval])
+            interval *= 2
+        return lists[0] if amount > 0 else None
+
+    def merge2Lists(self, l1, l2):
+        head = point = ListNode(0)
+        while l1 and l2:
+            if l1.val <= l2.val:
+                point.next = l1
+                l1 = l1.next
+            else:
+                point.next = l2
+                l2 = l1
+                l1 = point.next.next
+            point = point.next
+        if not l1:
+            point.next=l2
+        else:
+            point.next=l1
+        return head.next
         

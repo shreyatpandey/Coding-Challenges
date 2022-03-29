c237fe6756ebfe783d31624353070b4c1ca0f020
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-03-28 21:19:27 -0700

    Create Word Search.py

---
+++ Fb_ph/Practice/Python/Mock Coding/Word Search.py
@@ -0,0 +1,57 @@
+'''
+TC:- O(N.4^L)
+where N is the number of cells in the board
+L is the length of word to match
+and 4^L is the number of directions it has to move
+SC:-O(L) for the recursion stack and L is the length of the word
+
+'''
+
+
+class Solution(object):
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
+        self.ROWS = len(board)
+        self.COLS = len(board[0])
+        self.board = board
+
+        for row in range(self.ROWS):
+            for col in range(self.COLS):
+                if self.backtrack(row, col, word):
+                    return True
+
+        # no match found after all exploration
+        return False
+
+
+    def backtrack(self, row, col, suffix):
+        # bottom case: we find match for each letter in the word
+        dirs = [(-1,0),(0,-1),(0,1),(1,0)]
+        if len(suffix) == 0:
+            return True
+
+        # Check the current status, before jumping into backtracking
+        if row < 0 or row == self.ROWS or col < 0 or col == self.COLS \
+                or self.board[row][col] != suffix[0]:
+            return False
+
+        ret = False
+        # mark the choice before exploring further.
+        self.board[row][col] = '#'
+        # explore the 4 neighbor directions
+        for x,y in dirs:
+            xcor = row + x
+            ycor = col + y
+            ret = self.backtrack(xcor, ycor, suffix[1:])
+            # break instead of return directly to do some cleanup afterwards
+            if ret: break
+
+        # revert the change, a clean slate and no side-effect
+        self.board[row][col] = suffix[0]
+
+        # Tried all directions, and did not find any match
+        return ret

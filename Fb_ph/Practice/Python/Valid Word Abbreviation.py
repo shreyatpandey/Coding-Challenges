3b3ecfb9c96e0d94a373bab7ab1aabde6dce2e03
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-03-29 20:44:19 -0700

    Update Valid Word Abbreviation.py

--- Fb_ph/Practice/Python/Valid Word Abbreviation.py
+++ Fb_ph/Practice/Python/Valid Word Abbreviation.py
@@ -1,3 +1,17 @@
+'''
+A string can be abbreviated by replacing any number of non-adjacent, non-empty substrings with their lengths. 
+The lengths should not have leading zeros.
+
+For example, a string such as "substitution" could be abbreviated as (but not limited to):
+
+"s10n" ("s ubstitutio n")
+"sub4u4" ("sub stit u tion")
+"12" ("substitution")
+"su3i1u2on" ("su bst i t u ti on")
+"substitution" (no substrings replaced)
+
+'''
+
 class Solution:
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
         i,j = 0,0
@@ -17,12 +31,13 @@ class Solution:
                 i += 1
                 j += 1
         return i == len(word) and j== len(abbr)
-     
-s = Solution()
-print("TestCase-1:")
-print(s.validWordAbbreviation("internationalization","i12iz4n"))
-print("TestCase-2:")
-print(s.validWordAbbreviation("apple","a2e"))
-print("TestCase-3:")
-print(s.validWordAbbreviation("a","01"))
+
+if __name__ == '__main__':
+    s = Solution()
+    print("TestCase-1:")
+    print(s.validWordAbbreviation("internationalization","i12iz4n"))
+    print("TestCase-2:")
+    print(s.validWordAbbreviation("apple","a2e"))
+    print("TestCase-3:")
+    print(s.validWordAbbreviation("a","01"))
 

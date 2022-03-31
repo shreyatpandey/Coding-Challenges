bb15e58edf79aa0e87546dd3c5532f29a34bed07
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-03-31 08:42:17 -0700

    Update Word Break.py

--- Fb_ph/Practice/Python/Word Break.py
+++ Fb_ph/Practice/Python/Word Break.py
@@ -25,3 +25,18 @@ class Solution:
                         q.append(new_s)
                         seen.add(new_s)
         return False
+'''
+Approach: DFS
+Tc:- O(n^3)
+SC:- O(n)
+'''
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        @lru_cache
+        def helper(i):
+            if not s[i:]: return True
+            for word in wordDict:
+                if s[i:].startswith(word) and helper(i + len(word)):
+                    return True
+            return False
+        return helper(0)

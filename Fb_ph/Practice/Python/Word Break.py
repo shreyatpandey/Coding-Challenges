9313b2ccacfbe21b55603598b7d499ee34090ec4
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-03-31 08:38:42 -0700

    Create Word Break.py

---
+++ Fb_ph/Practice/Python/Word Break.py
@@ -0,0 +1,27 @@
+'''
+Input: s = "leetcode", wordDict = ["leet","code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+
+'''
+
+'''
+Approach: BFS
+Tc:- O(n^3)
+SC:- O(n)
+'''
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        q = deque([s])
+        seen = set() 
+        while q:
+            s = q.popleft()    # popleft() = BFS ; pop() = DFS
+            for word in wordDict:
+                if s.startswith(word):
+                    new_s = s[len(word):]
+                    if new_s == "": 
+                        return True
+                    if new_s not in seen:
+                        q.append(new_s)
+                        seen.add(new_s)
+        return False

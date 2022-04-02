fe2719cddbdee0b0a7b8766dbd1c8d3af2ce45d5
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-04-02 10:23:20 -0700

    Diagonal Traverse

--- Fb_ph/Practice/Python/Diagonal Traverse.py
+++ Fb_ph/Practice/Python/Diagonal Traverse.py
@@ -1,5 +1,6 @@
 '''
-We also need to keep track of whether we're traversing top-right or bottom-left, and accordingly set the starting and ending cells in the addDiagonal function call.
+We also need to keep track of whether we're traversing top-right or bottom-left, 
+and accordingly set the starting and ending cells in the addDiagonal function call.
 
 The only tricky part is updating the boundaries after we've added a diagonal, but I think the code is self-explanatory.
 '''
@@ -39,3 +40,36 @@ class Solution:
                 d = 1 - d
 
         return result
+    
+ '''
+ Approach-2: Using Map
+ TC:- O(n*m)
+ SC:- O(n) for the map
+ '''
+class Solution:
+    def findDiagonalOrder(self, matrix: List[List[int]]) -> List[int]:
+        if not matrix or not matrix[0]:
+            return []
+        
+        N= len(matrix)
+        M= len(matrix[0])
+        res = []
+        Dict = OrderedDict() #here its Ordered and not UnOrdered as order for traverdsal has to be maintained
+        for i in range(N):
+            for j in range(M):
+                Sum=i+j;
+                if Sum not in Dict:
+                    Dict[Sum] = []
+                Dict[Sum].append(matrix[i][j])
+     
+        for key,values in Dict.items():
+            tempList = []
+            if key%2 == 0:
+                temp_list= values
+                temp_list.reverse()
+            else:
+                temp_list = values
+            for k in range(len(temp_list)):
+                res.append(temp_list[k])
+        
+        return res

6664b0c84e40b1354e3773613aae43b7d468a2da
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-04-02 12:34:35 -0700

    Word Break II

---
+++ Fb_ph/Practice/Python/Word Break II.py
@@ -0,0 +1,44 @@
+'''
+Given a string s and a dictionary of strings wordDict, 
+add spaces in s to construct a sentence where each word is a valid dictionary word. 
+Return all such possible sentences in any order.
+
+Note that the same word in the dictionary may be reused multiple times in the segmentation. 
+
+Example 1:
+
+Input: s = "catsanddog", wordDict = ["cat","cats","and","sand","dog"]
+Output: ["cats and dog","cat sand dog"]
+
+Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+Output: []
+'''
+
+
+'''
+Let W be the number of words in WordDict
+N - length of the string
+TC:- O(N^2 + 2^N + W)
+SC:- O(2^N.N + W)
+'''
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        def helper(s, wordDict, memo):
+            if s in memo: return memo[s]
+            if not s: return []
+    
+            res = []
+            for word in wordDict:
+                if not s.startswith(word):
+                    continue
+                if len(word) == len(s):
+                    res.append(word)
+                else:
+                    resultOfTheRest = helper(s[len(word):], wordDict, memo)
+                    for item in resultOfTheRest:
+                        item = word + ' ' + item
+                        res.append(item)
+            memo[s] = res
+            return res
+
+        return helper(s,wordDict,{})

8c894b4832d5af340c454c497a8c483a58770b3d
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-04-03 10:46:23 -0700

    Update Dot Product of Sparse Vector.py

--- Fb_ph/Practice/Python/Dot Product of Sparse Vector.py
+++ Fb_ph/Practice/Python/Dot Product of Sparse Vector.py
@@ -72,4 +72,55 @@ class Solution(object):
             else:
                 i += 1
         return DotProduct
+ '''
+ Another Damn Follow-Up:-
+ what would you do if one of the vectors werent fully "sparse" -> hint use binary search
+ If the sizes of the two index-pair vectors are n and m then you iterate the smaller vector and binary search the larger one. 
+ So the effective runtime is O(min(n, m) * log(max(n, m)))
+ '''
+'''
+Approach-4:
+Tuple + Binary Search
+TC:- O(min(n,m)*log(max(n,m))
+SC:- O(n+m)
+'''
+class SparseVector:
+    def __init__(self, nums: List[int]):
+        self.nums = []
+        for i,num in enumerate(nums):
+            if num:
+                self.nums.append((i,num))
+        ''''
+        Write this only if interviewer ask about Binary - Search
+        self.indices = []
+        for idx in self.nums:
+            self.indices.append(idx[0])
+        '''
+
+    # Return the dotProduct of two sparse vectors
+    def dotProduct(self, vec: 'SparseVector') -> int:
+        DotProduct = 0
+        i,j = 0,0
+        while i<len(self.nums) and j<len(vec.nums):
+            i_idx,i_num = self.nums[i]
+            j_idx,j_num = vec.nums[j]
+            if i_idx == j_idx:
+                DotProduct += (i_num*j_num)
+                i += 1
+                j += 1
+            elif i_idx > j_idx:
+                #j += 1
+                #Advance j using binary search
+                j = bisect.bisect_left(vec.indices, i_idx, lo=j+1)
+            else:
+                #i += 1
+                # Advance i using binary search
+                i = bisect.bisect_left(self.indices, j_idx, lo=i+1)
+        return DotProduct
+        
+
+# Your SparseVector object will be instantiated and called as such:
+# v1 = SparseVector(nums1)
+# v2 = SparseVector(nums2)
+# ans = v1.dotProduct(v2)
      

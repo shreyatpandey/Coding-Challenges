c136b0644e613149bd6d76edd14bb5ca19be819f
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-04-04 08:23:37 -0700

    Update Custom Sort String.py

--- Fb_ph/Practice/Python/Custom Sort String.py
+++ Fb_ph/Practice/Python/Custom Sort String.py
@@ -1,4 +1,29 @@
 '''
+Question:-
+You are given two strings order and s. 
+All the words of order are unique and were sorted in some custom order previously.
+
+Permute the characters of s so that they match the order that order was sorted. 
+More specifically, if a character x occurs before a character y in order, then x should occur before y in the permuted string.
+
+Return any permutation of s that satisfies this property.
+'''
+'''
+Example 1:
+
+Input: order = "cba", s = "abcd"
+Output: "cbad"
+Explanation: 
+"a", "b", "c" appear in order, so the order of "a", "b", "c" should be "c", "b", and "a". 
+Since "d" does not appear in order, it can be at any position in the returned string. "dcba", "cdba", "cbda" are also valid outputs.
+'''
+'''
+Question to ask?
+Are there any duplicates in order string? Mostly they are unique
+Will input have null string?
+'''
+
+'''
 Time:- 45 ms
 '''
 class Solution:
@@ -14,8 +39,14 @@ class Solution:
                 stringResult += (char*getCount)
                 
         return stringResult
-      
+
  '''
+ Approach:- 1
+ Keep Track of Count and then write
+ Get the frequency of each character in s 
+ 
+ TC:- O(len(order) + O(len(s))
+ SC:- O(len(s)) for the frequency count 
  Time:- 43 ms
  '''
 class Solution:

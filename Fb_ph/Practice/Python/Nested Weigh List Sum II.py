0964898be73b165c5f189f99e8db11e1fb16d66c
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-04-04 07:50:54 -0700

    Create Nested Weigh List Sum II.py

---
+++ Fb_ph/Practice/Python/Nested Weigh List Sum II.py
@@ -0,0 +1,49 @@
+'''
+You are given a nested list of integers nestedList. Each element is either an integer or a list whose elements may also be integers or other lists.
+
+The depth of an integer is the number of lists that it is inside of. 
+For example, the nested list [1,[2,2],[[3],2],1] has each integer's value set to its depth. Let maxDepth be the maximum depth of any integer.
+
+The weight of an integer is maxDepth - (the depth of the integer) + 1.
+
+Return the sum of each integer in nestedList multiplied by its weight.
+'''
+'''
+Here the max depth is obtained by the formula:- maxDepth = Depth - 1
+
+'''
+'''
+Double Pass BFS
+
+'''
+class Solution:
+    def depthSumInverse(self, nestedList: List[NestedInteger]) -> int:
+        queue = deque(nestedList)
+        maxWeight = 0
+        Depth = 1
+        maxDepth = 0
+        while len(queue)>0:
+            Length = len(queue)
+            for i in range(Length):
+                nested = queue.pop()
+                if nested.isInteger() == False:
+                    queue.extendleft(nested.getList())
+            Depth += 1
+        
+        maxDepth = Depth-1
+        #print(maxDepth)
+        queue = deque(nestedList)
+        Depth = 1
+        totalSum = 0
+        while len(queue)>0:
+            Length = len(queue)
+            for i in range(Length):
+                nested = queue.pop()
+                if nested.isInteger() :
+                    diff = maxDepth - (Depth)+1
+                    totalSum += diff*nested.getInteger()
+                else:
+                    queue.extendleft(nested.getList())
+            Depth += 1
+        
+        return totalSum

f635d78343b12b00d9dabeba4e73031094956862
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-04-04 07:32:06 -0700

    Update Minimum Remove To Make Parenthesis Valid.py

--- Fb_ph/Practice/Python/Minimum Remove To Make Parenthesis Valid.py
+++ Fb_ph/Practice/Python/Minimum Remove To Make Parenthesis Valid.py
@@ -54,3 +54,31 @@ class Solution:
                     continue
             stringResult.append(char)
         return "".join(stringResult)
+
+ '''
+ TC:- O(N)
+ SC:- O(1) or O(N)
+ '''
+class Solution:
+    def minRemoveToMakeValid(self, s: str) -> str:
+        cnt_open, cnt_close, res = 0, 0, ''
+        for ch in s:
+            if ch == '(': cnt_open += 1
+            if ch == ')': cnt_close += 1
+            if cnt_open < cnt_close:
+                cnt_close -= 1
+            else:
+                res = res + ch
+
+        s = res
+        
+        cnt_open, cnt_close, res = 0, 0, ''
+        for ch in reversed(s):
+            if ch == '(': cnt_open += 1
+            if ch == ')': cnt_close += 1
+            if cnt_close < cnt_open:
+                cnt_open -= 1
+            else:
+                res = ch + res
+                
+        return res

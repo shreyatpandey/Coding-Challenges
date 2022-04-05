286bdb70d6380c464f3160570c374e82cd75e268
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-04-05 07:46:40 -0700

    Create Basic Calculator.py

---
+++ Fb_ph/Practice/Python/Basic Calculator.py
@@ -0,0 +1,45 @@
+'''
+Given a string s representing a valid expression, implement a basic calculator to evaluate it, and return the result of the evaluation.
+
+Note: You are not allowed to use any built-in function which evaluates strings as mathematical expressions, such as eval().
+
+ 
+
+Example 1:
+
+Input: s = "1 + 1"
+Output: 2
+Example 2:
+
+Input: s = " 2-1 + 2 "
+Output: 3
+Example 3:
+
+Input: s = "(1+(4+5+2)-3)+(6+8)"
+Output: 23
+'''
+'''
+TC:- O(n) for the length of the string
+SC:- O(n) for stack
+'''
+class Solution:
+    def calculate(self, s: str) -> int:
+        res, num, sign, stack = 0, 0, 1, []
+        for ss in s:
+            if ss.isdigit():
+                num = 10*num + int(ss)
+            elif ss in ["-", "+"]:
+                res += sign*num
+                num = 0
+                sign = 1 if ss =="+" else -1 
+            elif ss == "(":
+                stack.append(res)
+                stack.append(sign)
+                sign, res = 1, 0
+            elif ss == ")":
+                res += sign*num
+                res *= stack.pop()
+                res += stack.pop()
+                num = 0
+        return res + num*sign
+        

d4201b7bf34ebc46631378b45e259647986f4344
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-04-05 07:52:44 -0700

    Basic Calculator III

---
+++ Fb_ph/Practice/Python/Basic Calculator III.py
@@ -0,0 +1,60 @@
+'''
+Implement a basic calculator to evaluate a simple expression string.
+
+The expression string contains only non-negative integers, '+', '-', '*', '/' operators, and open '(' and closing parentheses ')'. The integer division should truncate toward zero.
+
+You may assume that the given expression is always valid. All intermediate results will be in the range of [-231, 231 - 1].
+
+Note: You are not allowed to use any built-in function which evaluates strings as mathematical expressions, such as eval().
+
+ 
+
+Example 1:
+
+Input: s = "1+1"
+Output: 2
+Example 2:
+
+Input: s = "6-4/2"
+Output: 4
+Example 3:
+
+Input: s = "2*(5+5*2)/3+(6/2+8)"
+Output: 21
+'''
+'''
+TC:- O(n) where n is the length of the string
+SC:- O(n) for the recursion stack
+'''
+class Solution:
+    def calculate(self, s: str) -> int:
+        s = s + "$"
+        def helper(stack, i):
+            num = 0
+            sign = '+'
+            while i < len(s):
+                c = s[i]
+                if c == " ":
+                    i += 1
+                    continue
+                if c.isdigit():
+                    num = 10 * num + int(c)
+                    i += 1
+                elif c == '(':
+                    num, i = helper([], i+1)
+                else:
+                    if sign == '+':
+                        stack.append(num)
+                    if sign == '-':
+                        stack.append(-num)
+                    if sign == '*':
+                        stack.append(stack.pop() * num)
+                    if sign == '/':
+                        stack.append(int(stack.pop() / num))
+                    num = 0
+                    i += 1
+                    if c == ')':
+                        return sum(stack), i
+                    sign = c 
+            return sum(stack)
+        return helper([], 0)

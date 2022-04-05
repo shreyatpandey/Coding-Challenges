36d1a91878c2b23825f46025deceb338af619c18
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-04-04 17:23:17 -0700

    Update Add Binary.py

--- Fb_ph/Practice/Python/Add Binary.py
+++ Fb_ph/Practice/Python/Add Binary.py
@@ -27,3 +27,30 @@ class Solution:
         answer.reverse()
         
         return ''.join(answer)
+'''
+Give this apporach only when asked 
+TC same as above
+'''
+'''Algorithm
+
+Convert a and b into integers x and y, x will be used to keep an answer, and y for the carry.
+
+While carry is nonzero: y != 0:
+
+Current answer without carry is XOR of x and y: answer = x^y.
+
+Current carry is left-shifted AND of x and y: carry = (x & y) << 1.
+
+Job is done, prepare the next loop: x = answer, y = carry.
+
+Return x in the binary form.
+
+'''
+class Solution:
+    def addBinary(self, a, b) -> str:
+        x, y = int(a, 2), int(b, 2)
+        while y:
+            answer = x ^ y
+            carry = (x & y) << 1
+            x, y = answer, carry
+        return bin(x)[2:]

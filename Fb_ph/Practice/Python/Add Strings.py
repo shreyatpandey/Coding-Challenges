dd60393c7a14cacfe143d012747c83abe1679c95
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-04-05 20:58:53 -0700

    Update Add Strings.py

--- Fb_ph/Practice/Python/Add Strings.py
+++ Fb_ph/Practice/Python/Add Strings.py
@@ -4,6 +4,11 @@ Given two non-negative integers, num1 and num2 represented as string, return the
 
 You must solve the problem without using any built-in library for handling large integers (such as BigInteger). 
 You must also not convert the inputs to integers directly.
+Constraints:
+
+1 <= num1.length, num2.length <= 104
+num1 and num2 consist of only digits.
+num1 and num2 don't have any leading zeros except for the zero itself.
 
 Example 1:
 

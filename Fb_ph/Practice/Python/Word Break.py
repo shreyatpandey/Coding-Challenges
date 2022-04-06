5fb3b2446b1a55a3358a428778ce12eada9e6724
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-04-05 20:51:24 -0700

    Update Word Break.py

--- Fb_ph/Practice/Python/Word Break.py
+++ Fb_ph/Practice/Python/Word Break.py
@@ -1,5 +1,6 @@
 '''
 Note the same word in word dict would be used multiple times
+the max size of the wordDict would be smaller than the s
 '''
 
 '''
@@ -44,3 +45,26 @@ class Solution:
                     return True
             return False
         return helper(0)
+ '''
+ Approach:- bottom-Up
+ TC:- O(m.n)
+ SC:- O(len(s)) which is the space occupied by the 1-D Dp matrix
+ Decision Tree->Cache->DP
+ Key is to first check whether the prefix in s startswith the first word in the wordDict
+ 
+ "leetcode , ["leet","code"]
+ i = 0 , i=4 are two important index as i = 0 is l for leet and i = 4 for code
+ dp[8] = True in leetcode as the length is 8
+ dp[0+len(w}] = True
+ '''
+def wordBreak(s,wordDict):
+    dp = [False]*len(s)+1
+    dp[len(s) = True #Base Case
+    for i in range(len(s)-1,-1,-1):
+       for w in wordDict:
+            if(i+len(w))<=len(s) and s[i:i+len(s)] == w:
+                dp[i] = dp[i+len(w)]
+            if dp[i] == True:
+                break
+    return dp[0]
+                

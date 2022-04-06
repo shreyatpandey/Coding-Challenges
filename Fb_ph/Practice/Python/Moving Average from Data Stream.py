76b146dfc7df0250b0e3510e1c1dd1457d72b21e
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-04-05 21:01:06 -0700

    Update Moving Average from Data Stream.py

--- Fb_ph/Practice/Python/Moving Average from Data Stream.py
+++ Fb_ph/Practice/Python/Moving Average from Data Stream.py
@@ -22,6 +22,11 @@ movingAverage.next(10); // return 5.5 = (1 + 10) / 2
 movingAverage.next(3); // return 4.66667 = (1 + 10 + 3) / 3
 movingAverage.next(5); // return 6.0 = (10 + 3 + 5) / 3
 
+Constraints:
+
+1 <= size <= 1000
+-105 <= val <= 105
+At most 104 calls will be made to next.
 
 '''
 

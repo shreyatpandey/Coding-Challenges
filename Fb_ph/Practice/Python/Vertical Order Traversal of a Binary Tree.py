dc550339ae0e7a730ac1c347dce1c258ad152e78
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-04-10 18:09:58 -0700

    Update Vertical Order Traversal of a Binary Tree.py

--- Fb_ph/Practice/Python/Vertical Order Traversal of a Binary Tree.py
+++ Fb_ph/Practice/Python/Vertical Order Traversal of a Binary Tree.py
@@ -1,4 +1,19 @@
 '''
+Question:-
+Given the root of a binary tree, calculate the vertical order traversal of the binary tree.
+
+For each node at position (row, col), its left and right children will be at positions (row + 1, col - 1) and (row + 1, col + 1) respectively. 
+The root of the tree is at (0, 0).
+
+The vertical order traversal of a binary tree is a list of top-to-bottom orderings for each column index starting from the leftmost column and ending on the rightmost column. 
+There may be multiple nodes in the same row and same column. In such a case, sort these nodes by their values.
+
+Return the vertical order traversal of the binary tree.
+
+'''
+
+
+'''
 TC:- O(Nlog(N/k)) where k is the width of the tree
 Partition Sorting
 In the first step , it takes O(N) TC for both BFS and DFS traversal

599774168f19fe9f36d9920772bbca3f92d2b1ba
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-04-11 07:53:22 -0700

    Update Sum Root to Leaf Numbers.py

--- Fb_ph/Practice/Python/Sum Root to Leaf Numbers.py
+++ Fb_ph/Practice/Python/Sum Root to Leaf Numbers.py
@@ -1,4 +1,29 @@
 '''
+You are given the root of a binary tree containing digits from 0 to 9 only.
+
+Each root-to-leaf path in the tree represents a number.
+
+For example, the root-to-leaf path 1 -> 2 -> 3 represents the number 123.
+Return the total sum of all root-to-leaf numbers. Test cases are generated so that the answer will fit in a 32-bit integer.
+
+A leaf node is a node with no children.
+
+Input: root = [1,2,3]
+Output: 25
+Explanation:
+The root-to-leaf path 1->2 represents the number 12.
+The root-to-leaf path 1->3 represents the number 13.
+Therefore, sum = 12 + 13 = 25.
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 1000].
+0 <= Node.val <= 9
+The depth of the tree will not exceed 10.
+'''
+
+
+'''
 Approach-1:- Preorder Iterative Traversal
 TC:- O(N)
 SC:- O(H) for Queue

ef64f9081470a0bd9bd0c46e772ff28befc968cb
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-04-12 08:43:06 -0700

    Create Robot Room Cleaner.py

---
+++ Fb_ph/Practice/Python/Robot Room Cleaner.py
@@ -0,0 +1,70 @@
+'''
+You are controlling a robot that is located somewhere in a room. 
+The room is modeled as an m x n binary grid where 0 represents a wall and 1 represents an empty slot.
+
+The robot starts at an unknown location in the room that is guaranteed to be empty, 
+and you do not have access to the grid, but you can move the robot using the given API Robot.
+
+You are tasked to use the robot to clean the entire room (i.e., clean every empty cell in the room). 
+The robot with the four given APIs can move forward, turn left, or turn right. Each turn is 90 degrees.
+
+When the robot tries to move into a wall cell, its bumper sensor detects the obstacle, and it stays on the current cell.
+
+Design an algorithm to clean the entire room using the following APIs:
+
+interface Robot {
+  // returns true if next cell is open and robot moves into the cell.
+  // returns false if next cell is obstacle and robot stays on the current cell.
+  boolean move();
+
+  // Robot will stay on the same cell after calling turnLeft/turnRight.
+  // Each turn will be 90 degrees.
+  void turnLeft();
+  void turnRight();
+
+  // Clean the current cell.
+  void clean();
+  
+Constraints:
+m == room.length
+n == room[i].length
+1 <= m <= 100
+1 <= n <= 200
+room[i][j] is either 0 or 1.
+0 <= row < m
+0 <= col < n
+room[row][col] == 1
+All the empty cells can be visited from the starting position.
+}
+'''
+# """
+# This is the robot's control interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class Robot:
+#    def move(self):
+#        """
+#        Returns true if the cell in front is open and robot moves into the cell.
+#        Returns false if the cell in front is blocked and robot stays in the current cell.
+#        :rtype bool
+#        """
+#
+#    def turnLeft(self):
+#        """
+#        Robot will stay in the same cell after calling turnLeft/turnRight.
+#        Each turn will be 90 degrees.
+#        :rtype void
+#        """
+#
+#    def turnRight(self):
+#        """
+#        Robot will stay in the same cell after calling turnLeft/turnRight.
+#        Each turn will be 90 degrees.
+#        :rtype void
+#        """
+#
+#    def clean(self):
+#        """
+#        Clean the current cell.
+#        :rtype void
+#        """

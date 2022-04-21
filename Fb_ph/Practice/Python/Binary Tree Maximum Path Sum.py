86d55d2b8dfa410d54e4da8a3107e4fa8f9bb4d3
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-04-21 07:52:21 -0700

    Update Binary Tree Maximum Path Sum.py

--- Fb_ph/Practice/Python/Binary Tree Maximum Path Sum.py
+++ Fb_ph/Practice/Python/Binary Tree Maximum Path Sum.py
@@ -5,12 +5,15 @@ A node can only appear in the sequence at most once. Note that the path does not
 The path sum of a path is the sum of the node's values in the path.
 
 Given the root of a binary tree, return the maximum path sum of any non-empty path.
-'''
-'''
-Constraints:-
+
 Input: root = [1,2,3]
 Output: 6
 Explanation: The optimal path is 2 -> 1 -> 3 with a path sum of 2 + 1 + 3 = 6.
+'''
+'''
+Constraints:-
+The number of nodes in the tree is in the range [1, 3 * 104].
+-1000 <= Node.val <= 1000
 '''
 '''
 Wording:-

d44a66885bb2418a34d4ed5988268610a547f97c
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-08-01 10:56:15 -0700

    Update Convert 1D Array into 2DArray.cpp

--- C++/Matrix/Convert 1D Array into 2DArray.cpp
+++ C++/Matrix/Convert 1D Array into 2DArray.cpp
@@ -16,3 +16,52 @@ public:
         return Result;
     }
 };
+//Square matrix without giving m and n would not work
+/*
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original) {
+        vector<vector<int>>Result(original.size(),vector<int>(original.size(),0));
+        int index = 0;
+        const int size = original.size();
+        while(index < original.size())
+        {
+            int rowIndex = index/size;
+            int colIndex = index%size;
+            Result[rowIndex][colIndex] = original[index];
+            index += 1;
+        }
+        return Result;
+    }
+};
+
+int main()
+{
+    vector<int>Mat = {{2,4,6,8,10,11,1,5,7}};
+    Solution s;
+    auto Result = s.construct2DArray(Mat);
+    for(int i=0;i<Result.size();i++)
+    {
+        for(int j=0;j<Result[i].size();j++)
+        {
+            cout<<Result[i][j]<<",";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+    return 0;
+}
+----| Output |----
+2,4,6,8,10,11,1,5,7,
+0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0,
+0,0,0,0,0,0,0,0,0
+*/

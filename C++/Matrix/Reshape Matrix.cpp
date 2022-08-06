4bbe13a3789b2928ad053e71995a5db3e00b56a6
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2022-08-06 10:30:33 -0700

    Update Reshape Matrix.cpp

--- C++/Matrix/Reshape Matrix.cpp
+++ C++/Matrix/Reshape Matrix.cpp
@@ -1,3 +1,17 @@
+/*
+You are given an m x n matrix mat and two integers r and c representing the number of rows and the number of columns of the wanted reshaped matrix.
+
+The reshaped matrix should be filled with all the elements of the original matrix in the same row-traversing order as they were.
+
+If the reshape operation with given parameters is possible and legal, output the new reshaped matrix; 
+Otherwise, output the original matrix.
+/* Hint-1*/
+M[i][j]=M[n*i+j] , where n is the number of cols. 
+This is the one way of converting 2-d indices into one 1-d index. Now, how will you convert 1-d index into 2-d indices?
+M[i] => M[i/n][i%n] 
+*/
+
+
 class Solution {
 public:
     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {

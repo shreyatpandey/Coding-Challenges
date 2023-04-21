d9c5c198ad68bcd83f9074f90221c76435c36d13
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2023-04-21 08:54:26 -0700

    Create Intger to Characcter[itoa].c

---
+++ Apple/NumericCharQuestions/Intger to Characcter[itoa].c
@@ -0,0 +1,37 @@
+/* itoa */
+#include<stdio.h>
+#define MAX 25600
+void ReverseInteger ( char InputStr[],int length)
+{
+  for(int i=0;i<length/2;i++)
+  {
+    char temp = InputStr[i];
+    InputStr[i] = InputStr[length-i-1];
+    InputStr[length-i-1] = temp;
+  }
+
+}
+
+int main()
+{
+  int InputVal = 35567 ;
+  int LengthString = 0;
+  char IntegerToChar [MAX];
+  while(InputVal)
+  {
+    int remainder = InputVal % 10;
+    InputVal /= 10;
+    if ( remainder > 9 )
+    {
+      IntegerToChar[LengthString++] = remainder + 55; //(int)'a' - 10
+    }
+    else
+    {
+      IntegerToChar[LengthString++] = remainder + 48; //(int)'0' - 48
+    }
+  }
+  IntegerToChar[LengthString] = '\0' ;
+  ReverseInteger(IntegerToChar,LengthString);
+  printf("IntegerToChar:%s\n",IntegerToChar);
+  return 0;
+}

32ce51554d74b50f1fd131d9ef4a8c126498a7eb
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2023-04-21 08:58:25 -0700

    Create Convert Decimal To Binary.cpp

---
+++ Apple/NumericCharQuestions/Convert Decimal To Binary.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    uint8_t eightBitArray[8];
+    uint8_t Val = 10;
+    /*8-Bit Integer*/
+    for(int i=7;i>=0;i--)
+    {
+        if((Val>>i)&1)
+         eightBitArray[i] = 1;
+         
+         else 
+            eightBitArray[i] = 0;
+    }
+  for(int i=7;i>=0;i--)
+  {
+      printf("%u",eightBitArray[i]);
+  }
+    
+    return 0;
+}
+
+/* Second method shift 1*/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    uint8_t eightBitArray[8];
+    uint8_t Val = 11;
+    /*8-Bit Integer*/
+    for(int i=7;i>=0;i--)
+    {
+        if((1<<i)&Val)
+         eightBitArray[i] = 1;
+         
+         else 
+            eightBitArray[i] = 0;
+    }
+  for(int i=7;i>=0;i--)
+  {
+      printf("%u",eightBitArray[i]);
+  }
+    
+    return 0;
+}

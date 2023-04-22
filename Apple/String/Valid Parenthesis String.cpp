fd058864f3fc2faf98d3346a32eefefbafae2fc3
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2023-04-21 21:20:40 -0700

    Create Valid Parenthesis String.cpp

---
+++ Apple/String/Valid Parenthesis String.cpp
@@ -0,0 +1,48 @@
+/*
+Given a string s containing only three types of characters: '(', ')' and '*', return true if s is valid.
+
+The following rules define a valid string:
+
+Any left parenthesis '(' must have a corresponding right parenthesis ')'.
+Any right parenthesis ')' must have a corresponding left parenthesis '('.
+Left parenthesis '(' must go before the corresponding right parenthesis ')'.
+'*' could be treated as a single right parenthesis ')' or a single left parenthesis '(' or an empty string "".
+*/
+
+class Solution {
+public:
+    bool checkValidString(string s) 
+    {
+    int leftBalance = 0;
+    for (int i=0; i<s.length(); i++) 
+    {
+      if ((s[i] == '(') || (s[i] == '*'))
+        leftBalance++;        
+      else
+        leftBalance--;
+      
+      if (leftBalance<0) return false; // We know we have unbalanced parenthesis
+    }
+    
+    // We can already check if parenthesis are valid
+    if (leftBalance == 0) return true;
+            
+    int rightBalance = 0;
+    for (int i=s.length()-1; i>=0; i--) {
+      if ((s[i] == ')') || (s[i] == '*'))
+        rightBalance++;
+      else
+        rightBalance--;
+      
+      if (rightBalance<0) return false;  // We know we have unbalanced parenthesis
+    }
+    
+    // Here we know we have never been unbalanced parsing from left to right e.g. ')('
+    // We've also already substituted '*' either by '(' or by ')'
+    // So we only have 3 possible scenarios here:
+    // 1. We had the same amount of '(' and ')'
+    // 2. We had more '(' then ')' but enough '*' to substitute
+    // 3. We had more ')' then '(' but enough '*' to substitute
+    return true;
+    }
+};

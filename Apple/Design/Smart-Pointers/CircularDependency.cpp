acd8aa1b1d1b115bba2ddb99e73a7980c57e6ce1
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2023-04-23 10:36:12 -0700

    CircularDependency.cpp

---
+++ Apple/Design/Smart-Pointers/CircularDependency.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<memory>
+using namespace std;
+
+class B;
+
+class A
+{
+    shared_ptr<B> sP1; // use weak_ptr instead to avoid CD
+
+public:
+    A() {  cout << "A()" << endl; }
+    ~A() { cout << "~A()" << endl; }
+
+    void setShared(shared_ptr<B>& p)
+    {
+        sP1 = p;
+    }
+};
+
+class B
+{
+    shared_ptr<A> sP1;
+
+public:
+    B() {  cout << "B()" << endl; }
+    ~B() { cout << "~B()" << endl; }
+
+    void setShared(shared_ptr<A>& p)
+    {
+        sP1 = p;
+    }
+};
+
+int main()
+{
+    shared_ptr<A> aPtr(new A);
+    shared_ptr<B> bPtr(new B);
+
+    aPtr->setShared(bPtr);
+    bPtr->setShared(aPtr);
+
+    return 0;  
+}

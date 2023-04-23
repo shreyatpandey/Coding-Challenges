210ca141f643dcc7bf4cd9f427c478be6c9fe331
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2023-04-23 10:18:42 -0700

    Update PracticeMacro.cpp

--- Apple/Macro-Basic/PracticeMacro.cpp
+++ Apple/Macro-Basic/PracticeMacro.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 /* Following macro can convert Little Endian to Big Endian*/
 #define Swap2Bytes(x) ((((x)>>8)&0x00ff)|((((x)<<8)&0xff00)))
-#define Swap4Bytes(x) ((((x)>>24)&0x000000FF)) | ((((x)>>8)&0x0000FF00))|\
+#define Swap4Bytes(x) ((((x)>>24)&0x000000FF) | ((((x)>>8)&0x0000FF00))|\
                         (((x)<<8)&0x00FF0000) | (((x)<<24)&0xFF000000))
                         
 #define Max(x,y) (x>y?x:y)

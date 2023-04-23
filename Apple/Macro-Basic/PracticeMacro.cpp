dc4809e3b131dea3b3449bb0008e745ae083a2a6
shreyatpandey <32083899+shreyatpandey@users.noreply.github.com>
2023-04-23 09:48:14 -0700

    Create PracticeMacro.cpp

---
+++ Apple/Macro-Basic/PracticeMacro.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<sstream>
+using namespace std;
+
+#define Swap2Bytes(x) ((((x)>>8)&0x00ff)|((((x)<<8)&0xff00)))
+#define Swap4Bytes(x) ((((x)>>24)&0x000000FF)) | ((((x)>>8)&0x0000FF00))|\
+                        (((x)<<8)&0x00FF0000) | (((x)<<24)&0xFF000000))
+                        
+#define Max(x,y) (x>y?x:y)
+
+int main()
+{
+    cout<<"Swap2Bytes:"<<Swap2Bytes(11)<<endl;
+    printf("%x\n", Swap2Bytes(11));
+
+    return 0;
+}

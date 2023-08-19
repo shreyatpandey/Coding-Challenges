2a66bbfb062403350f3440f43242524e489dd658
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-08-19 11:12:07 +0530

    Update Remove Link List Elements.cpp
    Adding test-cases and description

--- C++/Link List/Remove Link List Elements.cpp
+++ C++/Link List/Remove Link List Elements.cpp
@@ -6,6 +6,19 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+/*
+Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
+Input: head = [1,2,6,3,4,5,6], val = 6
+Output: [1,2,3,4,5]
+Example 2:
+
+Input: head = [], val = 1
+Output: []
+Example 3:
+
+Input: head = [7,7,7,7], val = 7
+Output: []
+*/
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {

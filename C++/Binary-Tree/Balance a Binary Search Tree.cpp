3ad222540428fdb1999b1ae355a505b7421fe3fc
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-08-21 08:23:34 +0530

    Update Balance a Binary Search Tree.cpp
    Adding description for this question

--- C++/Binary-Tree/Balance a Binary Search Tree.cpp
+++ C++/Binary-Tree/Balance a Binary Search Tree.cpp
@@ -9,6 +9,11 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+/*
+Given the root of a binary search tree, return a balanced binary search tree with the same node values. 
+If there is more than one answer, return any of them.
+A binary search tree is balanced if the depth of the two subtrees of every node never differs by more than 1.
+*/
 
 class Solution {
     private:

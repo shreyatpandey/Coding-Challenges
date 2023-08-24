1562a1691c294fa7630735351fb19bee5385049a
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-08-24 20:06:06 +0530

    Create Sample-1.cpp

---
+++ C++/Conceptual Basics/Casting/Sample-1.cpp
@@ -0,0 +1,10 @@
+Q1] 
+#include <iostream>
+int main() {
+   constexpr unsigned int id = 100;
+   unsigned char array[] = { id % 3, id % 5 };
+   std::cout
+      << static_cast<unsigned int>(array[0])
+      << static_cast<unsigned int>(array[1]) ;
+}
+//Output:- 10

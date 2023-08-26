8ce6b9ac38b90b5a30fe9d5fd6c3e35138cfa8a8
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-08-26 20:08:16 +0530

    Update Implement 2D Array.c
    Adding-Easier-Version-Of-2Alloc

--- Arista Networks/Implement 2D Array.c
+++ Arista Networks/Implement 2D Array.c
@@ -1,8 +1,21 @@
+/* Easier Approach */
+int** my2DAllocEasy(int rows, int cols)
+{
+  int **rowptr;
+  rowptr = (int**)malloc(sizeof(int*));
+  for(int i=0;i<rows;i++)
+  {
+      rowptr[i] = (int*)malloc(sizeof(int)*cols); 
+  }
+    return rowptr;
+}
+
+/* Tricky */
 int** my2DAiloc(int rows, int cols) 
 { 
   int i;
  int header = rows * sizeof(int*);
- int data = rows * cols * sizeof(lnt);
+ int data = rows * cols * sizeof(int);
  int** rowptr = (int**)malloc(header + data);
  if (rowptr == NULL) return NULL; 
  int* buf = (int*) (rowptr + rows);

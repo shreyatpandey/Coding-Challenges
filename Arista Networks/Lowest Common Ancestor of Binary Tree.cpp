910dba801a93c34533e06d0cd081626654bd7679
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-08-27 15:35:20 +0530

    Update Lowest Common Ancestor of Binary Tree.cpp

--- Arista Networks/Lowest Common Ancestor of Binary Tree.cpp
+++ Arista Networks/Lowest Common Ancestor of Binary Tree.cpp
@@ -7,6 +7,7 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
 // TC:- O(n) where n is the total number of nodes in tree
 // SC:- O(N) where n is the total number of nodes indicating the recursion stack
 

2d126ace450a8cf686d3b873e66854130613d65e
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-09-13 13:32:03 +0530

    Update Verify an Alien Dictionary.py

--- Fb_ph/Practice/Python/Verify an Alien Dictionary.py
+++ Fb_ph/Practice/Python/Verify an Alien Dictionary.py
@@ -1,18 +1,18 @@
 '''
-et NN be the length of order, and MM be the total number of characters in words.
+Let N be the length of order, and M be the total number of characters in words.
 
-Time complexity : O(M)O(M).
+Time complexity : O(M).
 
-Storing the letter-order relation of each letter takes O(N)O(N) time. 
+Storing the letter-order relation of each letter takes O(N) time. 
 For the nested for-loops, we examine each pair of words in the outer-loop and for the inner loop, we check each letter in the current word. 
 Therefore, we will iterate over all of letters in words.
 
 Taking both into consideration, the time complexity is O(M + N) 
-However, we know that Nis fixed as 2626. Therefore, the time complexity is O(M)
+However, we know that N is fixed as 26. Therefore, the time complexity is O(M)
 
 Space complexity : O(1)
 The only extra data structure we use is the hashmap/array that serves to store the letter-order relations for each word in order. 
-Because the length of order is fixed as 2626, this approach achieves constant space complexity.
+Because the length of order is fixed as 26, this approach achieves constant space complexity.
 
 '''
 

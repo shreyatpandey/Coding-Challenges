c70d913a5c30896fb2d04e9d26d4b9320af6dd69
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-09-13 17:25:31 +0530

    Update Range Sum BST.py

--- Fb_ph/Practice/Python/Range Sum BST.py
+++ Fb_ph/Practice/Python/Range Sum BST.py
@@ -1,4 +1,9 @@
 '''
+Given the root node of a binary search tree and two integers low and high, 
+return the sum of values of all nodes with a value in the inclusive range [low, high].
+'''
+
+'''
 Constraints:
 
 The number of nodes in the tree is in the range [1, 2 * 104].

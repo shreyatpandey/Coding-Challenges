eb2c7cac009ca3d42f644400b87f6732544e2dfd
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-09-14 16:06:24 +0530

    Update Word Search.py

--- Fb_ph/Practice/Python/Mock Coding/Word Search.py
+++ Fb_ph/Practice/Python/Mock Coding/Word Search.py
@@ -1,10 +1,26 @@
 '''
+Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+The word can be constructed from letters of sequentially adjacent cells,
+where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+'''
+'''
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+Output: true
+'''
+'''
+Constraints:-
+m == board.length
+n = board[i].length
+1 <= m, n <= 6
+1 <= word.length <= 15
+board and word consists of only lowercase and uppercase English letters.
+'''
+'''
 TC:- O(n*m*4^L)
 where m,n are the length of row and col in the board
 L is the length of word to match
 and 4^L is the number of directions it has to move
 SC:-O(L) for the recursion stack and L is the length of the word
-
 '''
 
 

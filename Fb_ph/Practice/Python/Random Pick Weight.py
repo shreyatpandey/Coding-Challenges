d581a8e14db7fe6cc73d1958c6d1a9318aa9c522
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-09-14 11:21:57 +0530

    Update Random Pick Weight.py

--- Fb_ph/Practice/Python/Random Pick Weight.py
+++ Fb_ph/Practice/Python/Random Pick Weight.py
@@ -1,5 +1,5 @@
 '''
-ou are given a 0-indexed array of positive integers w where w[i] describes the weight of the ith index.
+You are given a 0-indexed array of positive integers w where w[i] describes the weight of the ith index.
 
 You need to implement the function pickIndex(), 
 which randomly picks an index in the range [0, w.length - 1] (inclusive) and returns it. 
@@ -21,10 +21,6 @@ Explanation
 Solution solution = new Solution([1]);
 solution.pickIndex(); // return 0. The only option is to return 0 since there is only one element in w.
 '''
-
-
-
-
 
 # used for random number generation
 import random

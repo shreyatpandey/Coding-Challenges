f497a6cf1e22e900a3a28b43ee1bf148cef30bea
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-09-15 08:01:41 +0530

    Update ToeplitzMatrix.py
    Adding Constraints

--- Fb_ph/Practice/Python/ToeplitzMatrix.py
+++ Fb_ph/Practice/Python/ToeplitzMatrix.py
@@ -11,7 +11,14 @@ Hash 2 rows (so only 1 element needs to be loaded at a time) and compare the res
 Question:- Toeplitz matrix
 Given an m x n matrix, return true if the matrix is Toeplitz. Otherwise, return false.
 A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same elements.
-
+'''
+'''
+Input: matrix = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
+Output: true
+Explanation:
+In the above grid, the diagonals are:
+"[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]".
+In each diagonal all elements are the same, so the answer is True.
 '''
 def isToeplitzMatrix(self, m):
   for i in range(len(m) - 1):

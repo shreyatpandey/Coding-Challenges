c3cf4b09d587a1a47fd07b182823a014a627f636
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-09-16 12:19:03 +0530

    Create Product of Array Except itself.py
    Product of Array Except itself

---
+++ Fb_ph/Practice/Python/Product of Array Except itself.py
@@ -0,0 +1,48 @@
+'''
+Complexity:-
+TC:- O(N)
+SC:- O(N)
+'''
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # The length of the input array 
+        length = len(nums)
+        
+        # The left and right arrays as described in the algorithm
+        L, R, answer = [0]*length, [0]*length, [0]*length
+        
+        # L[i] contains the product of all the elements to the left
+        # Note: for the element at index '0', there are no elements to the left,
+        # so the L[0] would be 1
+        L[0] = 1
+        for i in range(1, length):
+            
+            # L[i - 1] already contains the product of elements to the left of 'i - 1'
+            # Simply multiplying it with nums[i - 1] would give the product of all 
+            # elements to the left of index 'i'
+            print("left_nums[i-1]:",nums[i-1])
+            print("left_L[i-1]:",L[i-1])
+            L[i] = nums[i - 1] * L[i - 1]
+        print(L)
+        # R[i] contains the product of all the elements to the right
+        # Note: for the element at index 'length - 1', there are no elements to the right,
+        # so the R[length - 1] would be 1
+        R[length - 1] = 1
+        for i in reversed(range(length - 1)):
+            
+            # R[i + 1] already contains the product of elements to the right of 'i + 1'
+            # Simply multiplying it with nums[i + 1] would give the product of all 
+            # elements to the right of index 'i'
+            print("right_nums[i+1]:",nums[i+1])
+            print("right_R[i+1]:",R[i+1])
+            R[i] = nums[i + 1] * R[i + 1]
+        print(R)
+        # Constructing the answer array
+        for i in range(length):
+            # For the first element, R[i] would be product except self
+            # For the last element of the array, product except self would be L[i]
+            # Else, multiple product of all elements to the left and to the right
+            answer[i] = L[i] * R[i]
+        
+        return answer

c29af14ffa87faf8c721a0559706b3981c1c250c
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-09-17 12:05:36 +0530

    Update Merge K-Sorted List.py

--- Fb_ph/Practice/Python/Merge K-Sorted List.py
+++ Fb_ph/Practice/Python/Merge K-Sorted List.py
@@ -1,8 +1,20 @@
 '''
+You are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+Merge all the linked-lists into one sorted linked-list and return it.
+'''
+'''
+Constraints:
+k == lists.length
+0 <= k <= 10^4
+0 <= lists[i].length <= 500
+-10^4 <= lists[i][j] <= 10^4
+lists[i] is sorted in ascending order.
+The sum of lists[i].length will not exceed 10^4.
+'''
+'''
 Approach:- Merge With Divide and Conquer
 TC:- O(NLogK)
-
-
+SC:- O(1)
 '''
 # Definition for singly-linked list.
 # class ListNode:

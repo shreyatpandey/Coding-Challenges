7ea1e1c59443ad6b03fb765cb191947f1a01c0d5
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-09-18 16:42:33 +0530

    Pointers101.cpp

---
+++ C++/Conceptual Basics/Pointers/Pointers101.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+int main() 
+{
+    int* A = 1; //Through error as int to int* conversion is not permissive
+    int value = 5;
+    int *Temp = &value;
+    int *Temp1 = new int(10);
+    std::cout<<*Temp<<std::endl;
+    std::cout<<*Temp1<<std::endl;
+    return 0;
+}

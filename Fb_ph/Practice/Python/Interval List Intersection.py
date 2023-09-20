06bff91ce4f7f903807ec8b6e2a846baa6d8abcb
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-09-20 09:27:22 +0530

    Interval List Intersection.py

---
+++ Fb_ph/Practice/Python/Interval List Intersection.py
@@ -0,0 +1,46 @@
+'''
+You are given two lists of closed intervals, firstList and secondList, where firstList[i] = [starti, endi] and secondList[j] = [startj, endj].
+Each list of intervals is pairwise disjoint and in sorted order.
+Return the intersection of these two interval lists.
+A closed interval [a, b] (with a <= b) denotes the set of real numbers x with a <= x <= b.
+
+The intersection of two closed intervals is a set of real numbers that are either empty or represented as a closed interval. 
+For example, the intersection of [1, 3] and [2, 4] is [2, 3].
+'''
+'''
+Input: firstList = [[0,2],[5,10],[13,23],[24,25]], secondList = [[1,5],[8,12],[15,24],[25,26]]
+Output: [[1,2],[5,5],[8,10],[15,23],[24,24],[25,25]]
+'''
+'''
+Constraints:
+0 <= firstList.length, secondList.length <= 1000
+firstList.length + secondList.length >= 1
+0 <= starti < endi <= 109
+endi < starti+1
+0 <= startj < endj <= 109 
+endj < startj+1
+'''
+'''
+Complexity:-
+TC:- O(M+N) where M is the size of A and N is the size of N
+SC:- O(M+N)
+'''
+class Solution(object):
+    def intervalIntersection(self, A, B):
+        """
+        :type A: List[List[int]]
+        :type B: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        result = []
+        i, j = 0, 0
+        while i < len(A) and j < len(B):
+            left = max(A[i][0], B[j][0])
+            right = min(A[i][1], B[j][1])
+            if left <= right:
+                result.append([left,right]);
+            if A[i][1] < B[j][1]:
+                i += 1
+            else:
+                j += 1
+        return result

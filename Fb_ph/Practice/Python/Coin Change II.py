f88d87bbd966de34c3fee77d4e3e1082fa4d4e9e
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-09-21 11:25:36 +0530

    Coin Change II.py

---
+++ Fb_ph/Practice/Python/Coin Change II.py
@@ -0,0 +1,35 @@
+'''
+You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+
+Return the number of combinations that make up that amount. If that amount of money cannot be made up by any combination of the coins, return 0.
+
+You may assume that you have an infinite number of each kind of coin.
+
+The answer is guaranteed to fit into a signed 32-bit integer.
+
+Example 1:
+
+Input: amount = 5, coins = [1,2,5]
+Output: 4
+Explanation: there are four ways to make up the amount:
+5=5
+5=2+2+1
+5=2+1+1+1
+5=1+1+1+1+1
+'''
+'''
+Complexity:-
+TC:- O(amountxlen(coins))
+SC:- O(amount)
+
+'''
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        
+        for coin in coins:
+            for j in range(coin, amount + 1):
+                dp[j] += dp[j - coin]
+        
+        return dp[amount]

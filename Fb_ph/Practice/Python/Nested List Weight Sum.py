40b84b4a334fd13050c1b6284a5cf9c56725bd77
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-09-21 16:00:15 +0530

    Nested List Weight Sum.py

--- Fb_ph/Practice/Python/Nested List Weight Sum.py
+++ Fb_ph/Practice/Python/Nested List Weight Sum.py
@@ -1,3 +1,26 @@
+'''
+You are given a nested list of integers nestedList. 
+Each element is either an integer or a list whose elements may also be integers or other lists.
+
+The depth of an integer is the number of lists that it is inside of. 
+For example, the nested list [1,[2,2],[[3],2],1] has each integer's value set to its depth.
+
+Return the sum of each integer in nestedList multiplied by its depth.
+'''
+'''
+Input: nestedList = [[1,1],2,[1,1]]
+Output: 10
+Explanation: Four 1's at depth 2, one 2 at depth 1. 1*2 + 1*2 + 2*1 + 1*2 + 1*2 = 10.
+'''
+'''
+Constraints:
+
+1 <= nestedList.length <= 50
+The values of the integers in the nested list is in the range [-100, 100].
+The maximum depth of any integer is less than or equal to 50.
+'''
+
+
 # """
 # This is the interface that allows for creating nested lists.
 # You should not implement it, or speculate about its implementation

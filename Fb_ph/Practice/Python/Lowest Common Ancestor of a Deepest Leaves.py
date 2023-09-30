fa9b2d7f9fd0e3686af987c8e5a12b97269bad5c
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-09-30 19:00:14 +0530

    Lowest Common Ancestor of a Deepest Leaves.py

--- Fb_ph/Practice/Python/Lowest Common Ancestor of a Deepest Leaves.py
+++ Fb_ph/Practice/Python/Lowest Common Ancestor of a Deepest Leaves.py
@@ -7,4 +7,40 @@ The node of a binary tree is a leaf if and only if it has no children
 The depth of the root of the tree is 0. if the depth of a node is d, the depth of each of its children is d + 1.
 The lowest common ancestor of a set S of nodes, is the node A with the largest depth such that every node in S is in the subtree with root A.
 '''
+'''
+Input: root = [3,5,1,6,2,0,8,null,null,7,4]
+Output: [2,7,4]
+Explanation: We return the node with value 2, colored in yellow in the diagram.
+The nodes coloured in blue are the deepest leaf-nodes of the tree.
+Note that nodes 6, 0, and 8 are also leaf nodes, but the depth of them is 2, but the depth of nodes 7 and 4 is 3.
+Example 2:
 
+Input: root = [1]
+Output: [1]
+Explanation: The root is the deepest node in the tree, and it's the lca of itself.
+'''
+'''
+Approach
+helper function return the deepest depth of descendants.
+deepest represent the deepest depth.
+'''
+'''
+Complexity:-
+TC:- O(N)
+SC:- O(H)
+'''
+class Solution:
+    def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        self.lca, self.deepest = None, 0
+        def helper(node, depth):
+            self.deepest = max(self.deepest, depth)
+            if not node:
+                return depth
+            left = helper(node.left, depth + 1)
+            right = helper(node.right, depth + 1)
+            if left == right == self.deepest:
+                self.lca = node
+            return max(left, right)
+        helper(root, 0)
+        return self.lca
+        

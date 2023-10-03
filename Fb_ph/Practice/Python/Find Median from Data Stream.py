b36ce55a6d26260a124dfc1ec11c52dbd60ea6a1
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-10-03 09:54:25 +0530

    Find Median from Data Stream.py

--- Fb_ph/Practice/Python/Find Median from Data Stream.py
+++ Fb_ph/Practice/Python/Find Median from Data Stream.py
@@ -23,6 +23,30 @@ If all integer numbers from the stream are in the range [0, 100], how would you 
 If 99% of all integer numbers from the stream are in the range [0, 100], how would you optimize your solution?
 '''
 '''
+The idea is to divide numbers into 2 balanced halves, one half low stores low numbers, 
+the other half high stores high numbers. 
+To access the median in O(1), 
+we need a data structure that give us the maximum of low half and the minimum of high half in O(1). 
+That's where maxHeap and minHeap come into play.
+
+We use maxHeap to store a half of low numbers, top of the maxHeap is the highest number among low numbers.
+We use minHeap to store a half of high numbers, top of the minHeap is the lowest number among high numbers.
+We need to balance the size between maxHeap and minHeap while processing. Hence after adding k elements,
+If k = 2 * i then maxHeap.size = minHeap.size = i
+If k = 2 * i + 1, let maxHeap store 1 element more than minHeap, then maxHeap.size = minHeap.size + 1.
+
+When adding a new number num into our MedianFinder:
+Firstly, add num to the maxHeap, now maxHeap may contain the big element (which should belong to minHeap). 
+So we need to balance, by removing the highest element from maxHeap, and offer it to minHeap.
+Now, the minHeap might hold more elements than maxHeap, in that case, we need to balance the size, 
+by removing the lowest element from minHeap and offer it back to maxHeap.
+
+When doing findMedian():
+If maxHeap.size > minHeap.size return top of the maxHeap, which is the highest number amongs low numbers.
+Else if maxHeap.size == minHeap return the (maxHeap.top() + minHeap.top()) / 2
+
+'''
+'''
 Complexity
 Time:
 Constructor: O(1)
@@ -37,20 +61,24 @@ class MedianFinder(object):
         """
         initialize your data structure here.
         """
-        self.heaps = None, [], []
-        self.i = 1
+        self.minHeap = []
+        self.maxHeap = []
 
     def addNum(self, num):
         """
         :type num: int
         :rtype: None
         """
-        heappush(self.heaps[-self.i], -heappushpop(self.heaps[self.i], num * self.i))
-        self.i *= -1
+        heappush(self.maxHeap, -num)
+        heappush(self.minHeap, -heappop(self.maxHeap))
+        if len(self.minHeap) > len(self.maxHeap):
+            heappush(self.maxHeap, -heappop(self.minHeap))
         
 
     def findMedian(self):
         """
         :rtype: float
         """
-        return (self.heaps[self.i][0] * self.i - self.heaps[-1][0]) / 2.0
+        if len(self.maxHeap) > len(self.minHeap):
+            return -self.maxHeap[0]
+        return (-self.maxHeap[0] + self.minHeap[0]) / 2

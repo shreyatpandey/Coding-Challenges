d6c25838dcf43f117f242e8799c72b1db576eb06
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-10-07 10:15:20 +0530

    Intersection of Three Sorted Arrays.py

---
+++ Fb_ph/Practice/Python/Intersection of Three Sorted Arrays.py
@@ -0,0 +1,47 @@
+'''
+Given three integer arrays arr1, arr2 and arr3 sorted in strictly increasing order, 
+return a sorted array of only the integers that appeared in all three arrays.
+'''
+'''
+Example 1:
+
+Input: arr1 = [1,2,3,4,5], arr2 = [1,2,5,7,9], arr3 = [1,3,4,5,8]
+Output: [1,5]
+Explanation: Only 1 and 5 appeared in the three arrays.
+Example 2:
+
+Input: arr1 = [197,418,523,876,1356], arr2 = [501,880,1593,1710,1870], arr3 = [521,682,1337,1395,1764]
+Output: []
+'''
+'''
+Complexity:-
+TC:- O(n) total length of input arrays
+SC:- O(1)
+not counting answer list
+'''
+class Solution(object):
+    def arraysIntersection(self, arr1, arr2, arr3):
+        """
+        :type arr1: List[int]
+        :type arr2: List[int]
+        :type arr3: List[int]
+        :rtype: List[int]
+        """
+        ans = []
+        # prepare three pointers to iterate through three arrays
+        # p1, p2, and p3 point to the beginning of arr1, arr2, and arr3 accordingly
+        p1 = p2 = p3 = 0
+        while p1 < len(arr1) and p2 < len(arr2) and p3 < len(arr3):
+            if arr1[p1] == arr2[p2] == arr3[p3]:
+                ans.append(arr1[p1])
+                p1 += 1
+                p2 += 1
+                p3 += 1
+            else:
+                if arr1[p1] < arr2[p2]:
+                    p1 += 1
+                elif arr2[p2] < arr3[p3]:
+                    p2 += 1
+                else:
+                    p3 += 1
+        return ans

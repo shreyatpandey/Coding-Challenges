fb7c54f56b128effd1feeebf31eb5fa5f2a19865
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-10-08 10:05:35 +0530

    Spiral Matrix.py

---
+++ Fb_ph/Practice/Python/Spiral Matrix.py
@@ -0,0 +1,63 @@
+'''
+Approach:-
+Simulation
+Two pointers
+Taking care of the boundary of the matrix
+There are 4 boundaries that is up, down, right, left
+This problem can be divided into 4 sub divisions
+move right: (row, col + 1)
+move downwards: (row + 1, col)
+move left: (row, col - 1)
+move upwards: (row - 1, col)
+'''
+
+'''
+Complexity:-
+TC:- O(m*n) 
+where m is the number of columns and n is the number of rows
+SC:- O(1)
+'''
+class Solution(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        rowLength = len(matrix)
+        colLength = len(matrix[0])
+        SpiralMatrix = []
+        i , j = 0,0
+        while rowLength>0 and colLength>0:
+            for k in range(0,colLength): #topRow
+                SpiralMatrix.append(matrix[i][j])
+                j += 1
+            j -= 1
+            rowLength -= 1
+            if rowLength == 0:
+                break;
+            for k in range(0,rowLength): #rightmost Column Boundary
+                i += 1
+                SpiralMatrix.append(matrix[i][j])
+            
+            colLength -= 1
+            if colLength == 0:
+                break;
+        
+            for k in range(0,colLength): #BottomMost Row
+                j -= 1
+                SpiralMatrix.append(matrix[i][j])
+            
+            rowLength -= 1
+            if rowLength == 0:
+                break;
+            
+            for k in range(0,rowLength): #leftmost Column Boundary
+                i -= 1
+                SpiralMatrix.append(matrix[i][j])
+                
+            colLength -= 1
+            if colLength == 0:
+                break;
+            j += 1
+        
+        return SpiralMatrix

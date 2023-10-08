ab393d3119ce6ac71075199688341143b9ad7206
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-10-08 09:43:47 +0530

    Clone Graph.py

--- Fb_ph/Practice/Python/Clone Graph.py
+++ Fb_ph/Practice/Python/Clone Graph.py
@@ -30,6 +30,13 @@ Output: [[2,4],[1,3],[2,4],[1,3]]
 I don't think this is because it's hard or confusing, etc. 
 The problem is that the problem states that we're given a node as input, whereas the test cases / submission actually pass you a list of edges.
 '''
+'''
+The number of nodes in the graph is in the range [0, 100].
+1 <= Node.val <= 100
+Node.val is unique for each node.
+There are no repeated edges and no self-loops in the graph.
+The Graph is connected and all nodes can be visited starting from the given node.
+'''
 
 
 """

c03710b1233c09a3f7d4384318c88eec3e6590d5
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-10-08 11:31:20 +0530

    Happy Number.py

---
+++ Fb_ph/Practice/Python/Happy Number.py
@@ -0,0 +1,66 @@
+'''
+Write an algorithm to determine if a number n is happy.
+
+A happy number is a number defined by the following process:
+
+Starting with any positive integer, replace the number by the sum of the squares of its digits.
+Repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+Those numbers for which this process ends in 1 are happy.
+Return true if n is a happy number, and false if not.
+'''
+'''
+Example 1:
+
+Input: n = 19
+Output: true
+Explanation:
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+'''
+'''
+Constraints:
+1 <= n <= 2^31 - 1
+'''
+'''
+Approach:- Floyd's Cycling Algorithm
+Complexity:-
+Tc:- O(log(n))
+Sc:- O(1)
+'''
+def isHappy(self, n: int) -> bool:  
+    def get_next(number):
+        total_sum = 0
+        while number > 0:
+            number, digit = divmod(number, 10)
+            total_sum += digit ** 2
+        return total_sum
+
+    slow_runner = n
+    fast_runner = get_next(n)
+    while fast_runner != 1 and slow_runner != fast_runner:
+        slow_runner = get_next(slow_runner)
+        fast_runner = get_next(get_next(fast_runner))
+    return fast_runner == 1
+
+'''
+Approach:- HashSet
+Complexity
+Tc:- O(log(n))
+Sc:- O(log(n) for hashSet
+'''
+def isHappy(self, n: int) -> bool:
+    def get_next(n):
+        total_sum = 0
+        while n > 0:
+            n, digit = divmod(n, 10)
+            total_sum += digit ** 2
+        return total_sum
+
+    seen = set()
+    while n != 1 and n not in seen:
+        seen.add(n)
+        n = get_next(n)
+
+    return n == 1

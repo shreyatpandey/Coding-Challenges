4b11e120d284e464f35f3cf22228d83624241d09
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-10-08 10:06:49 +0530

    Spiral Matrix.py

--- Fb_ph/Practice/Python/Spiral Matrix.py
+++ Fb_ph/Practice/Python/Spiral Matrix.py
@@ -1,4 +1,19 @@
 '''
+Given a matrix, return the matrix in a spiral order
+'''
+
+'''
+Input:-
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [1,2,3,6,9,8,7,4,5]
+'''
+'''
+m == matrix.length
+n == matrix[i].length
+1 <= m, n <= 10
+-100 <= matrix[i][j] <= 100
+'''
+'''
 Approach:-
 Simulation
 Two pointers

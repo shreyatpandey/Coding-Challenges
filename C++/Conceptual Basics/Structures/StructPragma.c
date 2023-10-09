1ef3a928c20338bb014304d19ea54f2faa3e80ae
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-10-09 19:31:16 +0530

    StructPragma.c

--- C++/Conceptual Basics/Structures/StructPragma.c
+++ C++/Conceptual Basics/Structures/StructPragma.c
@@ -27,7 +27,7 @@ int main()
     struct packing_pragma pragma_case;
     struct packing_attribute attribute;
     
-    printf("Size of A is: %ld\n", sizeof(A));
+    printf("Size of A is: %ld\n", sizeof(A)); // round it off the next highest power of 2
     printf("Size of packing_pragma is:%ld\n",sizeof(pragma_case));
     printf("Size of packing_attribute is:%ld\n",sizeof(attribute)); 
     printf("Size of int:%ld\n",sizeof(int));
@@ -35,3 +35,12 @@ int main()
     printf("Size of char:%d\n",sizeof(char));
     printf("Hence size of struct is sum of all datatypes inside struct\n");
 } 
+/*
+Size of A is: 16
+Size of packing_pragma is:13
+Size of packing_attribute is:13
+Size of int:4
+Size of double:8
+Size of char:1
+Hence size of struct is sum of all datatypes inside struct
+*/

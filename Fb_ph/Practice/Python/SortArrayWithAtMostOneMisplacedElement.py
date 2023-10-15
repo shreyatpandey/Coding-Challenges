020ef7d7b7eb99c347c86bd9c0b8ffc963de4fe3
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-10-15 21:48:55 +0530

    SortArrayWithAtMostOneMisplacedElement.py

---
+++ Fb_ph/Practice/Python/SortArrayWithAtMostOneMisplacedElement.py
@@ -0,0 +1,56 @@
+'''
+A sorted array with at most one misplaced element
+'''
+'''
+Check if the index is given or not
+'''
+'''
+Complexity:-
+Tc:- O(n)
+Sc:- O(1)
+'''
+def misplacedElement(arr):
+    
+    # Traversing element of array.
+    n = len(arr)
+    pos = -1
+    for i in range(1,len(arr)):
+        if arr[i] < arr[i-1]:
+            pos = i
+    if pos == -1:
+        return 
+    
+    for i in range(n):
+        # For the element on left of pos
+        if (i < pos):
+            if (arr[pos] < arr[i]):
+                temp = arr[pos]
+                arr[pos] = arr[i]
+                arr[i] = temp
+ 
+        # For the element on right of pos
+        elif (i > pos):
+            if (arr[pos] > arr[i]):
+                temp = arr[pos]
+                arr[pos] = arr[i]
+                arr[i] = temp
+                pos = i
+    print("listInput:",arr)
+
+if __name__ == '__main__':
+    print("Test Case:-1")
+    misplacedElement([1,3,5])
+    
+    print("Test Case:-2")
+    misplacedElement([2,6,3,4,7,9])
+    
+    print("Test Case:-3")
+    misplacedElement([2,3,6,7,9,4])
+    
+    print("Test Case-4:")
+    misplacedElement([4,5,6,7,1])
+    
+    print("Test Case-5:")
+    misplacedElement([4,5,6,7,8,9,11,12,13,14,3])
+    
+    

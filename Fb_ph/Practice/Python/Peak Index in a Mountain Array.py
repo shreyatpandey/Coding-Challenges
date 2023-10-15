93873bba9736e1f08c5197df409a773069ad196b
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-10-15 13:08:46 +0530

    Peak Index in a Mountain Array.py

---
+++ Fb_ph/Practice/Python/Peak Index in a Mountain Array.py
@@ -0,0 +1,24 @@
+'''
+An array arr is a mountain if the following properties hold:
+
+arr.length >= 3
+There exists some i with 0 < i < arr.length - 1 such that:
+arr[0] < arr[1] < ... < arr[i - 1] < arr[i] 
+arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
+Given a mountain array arr, return the index i such that arr[0] < arr[1] < ... < arr[i - 1] < arr[i] > arr[i + 1] > ... > arr[arr.length - 1].
+'''
+'''
+Complexity:-
+Tc:- O(log(n))
+Sc:- O(1)
+'''
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        l, r = 0, len(arr) - 1
+        while l < r:
+            m = (l + r) // 2
+            if arr[m] < arr[m + 1]:
+                l = m + 1
+            else:
+                r = m
+        return l

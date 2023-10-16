1e7ac6885900fa9ee6a766b8657c6c4111ec54ee
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-10-16 12:25:55 +0530

    Intersection of Two Arrays.py

--- Fb_ph/Practice/Python/Intersection of Two Arrays.py
+++ Fb_ph/Practice/Python/Intersection of Two Arrays.py
@@ -20,3 +20,29 @@ class Solution:
             if num in set1 and num in set2:
                 Result.append(num)
         return Result
+
+'''
+Complexity:-
+Tc:- O(n*log(n))
+Sc:- O(1)
+'''
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1 = sorted(nums1)
+        nums2 = sorted(nums2)
+        p1 = 0
+        p2 = 0
+        
+        r = set()
+        while p1 < len(nums1) and p2 < len(nums2):
+            val1 = nums1[p1]
+            val2 = nums2[p2]
+            if val1 == val2:
+                r.add(val1)
+                p1 += 1
+            elif val1 < val2:
+                p1 +=1
+            elif val1 > val2:
+                p2 += 1
+                
+        return r

fa982067a13ac6a262cae3ace61b247df7936cbe
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-10-17 10:02:08 +0530

    Trapping Rain Water.py

--- Fb_ph/Practice/Python/Trapping Rain Water.py
+++ Fb_ph/Practice/Python/Trapping Rain Water.py
@@ -1,11 +1,19 @@
 '''
-Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+Given n non-negative integers representing an elevation map where the width of each bar is 1, 
+compute how much water it can trap after raining.
 '''
 '''
 Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
 Output: 6
 Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. 
 In this case, 6 units of rain water (blue section) are being trapped.
+'''
+'''
+So, we can say that if there is a larger bar at one end (say right), we are assured that the water trapped 
+would be dependant on height of bar in current direction (from left to right). 
+As soon as we find the bar at other end (right) is smaller, we start iterating in opposite direction (from right to left).
+We must maintain left_max and right_max during the iteration, 
+but now we can do it in one iteration using 2 pointers, switching between the two.
 '''
 '''
 Two Pointer Approach

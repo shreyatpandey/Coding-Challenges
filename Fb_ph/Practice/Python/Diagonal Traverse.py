37727afd04b1834f9b23dad208ac8b83e3d9e436
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-10-21 12:30:42 +0530

    Diagonal Traverse.py

--- Fb_ph/Practice/Python/Diagonal Traverse.py
+++ Fb_ph/Practice/Python/Diagonal Traverse.py
@@ -1,3 +1,10 @@
+
+'''
+Given an m x n matrix mat, return an array of all the elements of the array in a diagonal order.
+Input: mat = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [1,2,4,7,5,3,6,8,9]
+'''
+
 '''
 We also need to keep track of whether we're traversing top-right or bottom-left, 
 and accordingly set the starting and ending cells in the addDiagonal function call.

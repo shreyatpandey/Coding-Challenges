87f93865783bfb5212c0bb49996c17af7e3fd018
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-10-21 12:23:20 +0530

    Binary Tree Zig Zag Traversal.py

---
+++ Fb_ph/Practice/Python/Binary Tree Zig Zag Traversal.py
@@ -0,0 +1,33 @@
+'''
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[20,9],[15,7]]
+'''
+class Solution(object):
+    def zigzagLevelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        Queue = []
+        ZigZagOrderTraversal = []
+        if not root:
+            return ZigZagOrderTraversal;
+        Queue.append(root)
+        level = 0
+        while len(Queue) != 0:
+            LenQueue = len(Queue)
+            ZigZagOrderTraversal.append([])
+            i = 0
+            while i<LenQueue :
+                node = Queue[0]
+                del Queue[0]
+                ZigZagOrderTraversal[level].append(node.val)
+                if node.left:
+                    Queue.append(node.left)
+                if node.right:
+                    Queue.append(node.right)
+                i += 1
+            if (level & 1) == 1:
+                ZigZagOrderTraversal[level].reverse()
+            level += 1
+        return ZigZagOrderTraversal

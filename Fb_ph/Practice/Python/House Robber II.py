d18436049c0c51a5076da034339a2929f15baa02
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-10-21 10:38:25 +0530

    House Robber II.py

--- Fb_ph/Practice/Python/House Robber II.py
+++ Fb_ph/Practice/Python/House Robber II.py
@@ -1,3 +1,30 @@
+'''
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed. 
+All houses at this place are arranged in a circle. 
+That means the first house is the neighbor of the last one. Meanwhile, adjacent houses have a security system connected, 
+and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given an integer array nums representing the amount of money of each house, 
+return the maximum amount of money you can rob tonight without alerting the police.
+'''
+'''
+Example 1:
+
+Input: nums = [2,3,2]
+Output: 3
+Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2), because they are adjacent houses.
+'''
+'''
+Constraints:
+
+1 <= nums.length <= 100
+0 <= nums[i] <= 1000
+'''
+'''
+Complexity:-
+Tc:- O(n)
+Sc:- O(1)
+'''
 class Solution:
     def rob(self, nums: List[int]) -> int:
         return max(nums[0], self.helper(nums[1:]), self.helper(nums[:-1]))
@@ -10,3 +37,28 @@ class Solution:
             rob1 = rob2
             rob2 = newRob
         return rob2
+
+'''
+Complexity:-
+Tc:- O(n)
+Sc:- O(n)
+'''
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def houseRobber(nums):
+            # dynamic programming - decide each problem by its sub-problems:
+            dp = [0]*len(nums)
+            dp[0] = nums[0]
+            dp[1] = max(nums[0], nums[1])
+            for i in range(2, len(nums)):
+                dp[i] = max(dp[i-1], nums[i]+dp[i-2])
+
+            return dp[-1]
+        
+        # edge cases:
+        if len(nums) == 0: return 0
+        if len(nums) == 1: return nums[0]
+        if len(nums) == 2: return max(nums)
+        
+        # either use first house and can't use last or last and not first:
+        return max(houseRobber(nums[:-1]), houseRobber(nums[1:]))

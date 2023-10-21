18e9d24362a89da7110f35d3a4526697710d6d02
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-10-21 12:13:27 +0530

    Find First and Last Position in a Sorted Array.py

---
+++ Fb_ph/Practice/Python/Find First and Last Position in a Sorted Array.py
@@ -0,0 +1,47 @@
+'''
+Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+
+If target is not found in the array, return [-1, -1].
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+'''
+'''
+Constraints:
+
+0 <= nums.length <= 10^5
+-10^9 <= nums[i] <= 10^9
+nums is a non-decreasing array.
+-10^9 <= target <= 10^9
+'''
+'''
+Complexity
+Tc:- O(log(n))
+Sc:- O(1)
+'''
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        def search(x):
+            lo, hi = 0, len(nums)           
+            while lo < hi:
+                mid = (lo + hi) // 2
+                if nums[mid] < x:
+                    lo = mid+1
+                else:
+                    hi = mid                    
+            return lo
+        
+        lo = search(target)
+        hi = search(target+1)-1
+        
+        if lo <= hi:
+            return [lo, hi]
+                
+        return [-1, -1]
+        

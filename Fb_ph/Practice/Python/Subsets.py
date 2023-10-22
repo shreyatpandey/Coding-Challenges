fa83ffc520a2d1c29f64b17e0ddd537ededd146b
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-10-22 08:43:20 +0530

    Subsets.py

--- Fb_ph/Practice/Python/Subsets.py
+++ Fb_ph/Practice/Python/Subsets.py
@@ -36,6 +36,17 @@ class Solution(object):
             subset.pop()
 
 '''
+Approach:- Iterative
+Tc:- O(N.2^N)
+Sc:- O(1)
+'''
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = [[]]
+        for num in nums:
+            result += [item+[num]for item in result]
+        return result
+'''
 Approach:- Bit Manipulation
 Tc:- O(N.2^N)
 Sc:- O(1)

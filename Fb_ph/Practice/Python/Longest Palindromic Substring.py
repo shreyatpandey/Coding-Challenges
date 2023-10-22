c737469b81f02ebfd353d9e8e319720cf00fbf2c
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-10-22 08:03:17 +0530

    Longest Palindromic Substring.py

---
+++ Fb_ph/Practice/Python/Longest Palindromic Substring.py
@@ -0,0 +1,77 @@
+'''
+Given a string s, return the longest 
+palindromic substring in s.
+
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+'''
+'''
+Constraints:
+
+1 <= s.length <= 1000
+s consist of only digits and English letters.
+'''
+'''
+Approach:- Expand around center
+Tc:- O(n^2)
+Sc:- O(1)
+'''
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        def expand(i, j):
+            left = i
+            right = j
+            
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+                
+            return right - left - 1
+        
+        ans = [0, 0]
+
+        for i in range(len(s)):
+            odd_length = expand(i, i)
+            if odd_length > ans[1] - ans[0] + 1:
+                dist = odd_length // 2
+                ans = [i - dist, i + dist]
+
+            even_length = expand(i, i + 1)
+            if even_length > ans[1] - ans[0] + 1:
+                dist = (even_length // 2) - 1
+                ans = [i - dist, i + 1 + dist]
+                
+        i, j = ans
+        return s[i:j + 1]
+
+'''
+Approach:- DP
+Tc:- O(n^2)
+Sc:- O(n)
+'''
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        dp = [[False] * n for _ in range(n)]
+        ans = [0, 0]
+        
+        for i in range(n):
+            dp[i][i] = True
+        
+        for i in range(n - 1):
+            if s[i] == s[i + 1]:
+                dp[i][i + 1] = True
+                ans = [i, i + 1]
+
+        for diff in range(2, n):
+            for i in range(n - diff):
+                j = i + diff
+                if s[i] == s[j] and dp[i + 1][j - 1]:
+                    dp[i][j] = True
+                    ans = [i, j]
+
+        i, j = ans
+        return s[i:j + 1]

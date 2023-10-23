94ff980d4dfd61ac8c64d968e3ec7dfa3b61e5ef
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-10-23 10:18:49 +0530

    Flatten a Multilevel Doubly Linked List.py

--- Fb_ph/Practice/Python/Flatten a Multilevel Doubly Linked List.py
+++ Fb_ph/Practice/Python/Flatten a Multilevel Doubly Linked List.py
@@ -1,5 +1,20 @@
-Recursive:
+'''
+You are given a doubly linked list, which contains nodes that have a next pointer, a previous pointer, 
+and an additional child pointer. 
+This child pointer may or may not point to a separate doubly linked list, also containing these special nodes. 
+These child lists may have one or more children of their own, and so on, 
+to produce a multilevel data structure as shown in the example below.
 
+Given the head of the first level of the list, flatten the list so that all the nodes appear in a single-level, doubly linked list. 
+Let curr be a node with a child list. 
+The nodes in the child list should appear after curr and before curr.next in the flattened list.
+
+Return the head of the flattened list. The nodes in the list must have all of their child pointers set to null.
+'''
+
+'''
+Recursive:
+'''
 class Solution:
     def flatten(self, head: 'Node') -> 'Node':
         if not head: return None
@@ -19,8 +34,10 @@ class Solution:
             return tail
         travel(head)
         return head
-DFS with stack:
 
+'''
+DFS with stack
+'''
 class Solution:
     def flatten(self, head: 'Node') -> 'Node':
         if not head: return None

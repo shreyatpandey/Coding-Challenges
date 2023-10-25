5dfc896fb85745418f9d26c6e3f7e7793e570cf2
Shreyat <32083899+shreyatpandey@users.noreply.github.com>
2023-10-25 11:44:37 +0530

    First Missing Positive.py

---
+++ Fb_ph/Practice/Python/First Missing Positive.py
@@ -0,0 +1,37 @@
+'''
+Given an unsorted integer array nums, return the smallest missing positive integer.
+
+You must implement an algorithm that runs in O(n) time and uses O(1) auxiliary space.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,0]
+Output: 3
+Explanation: The numbers in the range [1,2] are all in the array.
+
+Example 2:
+
+Input: nums = [3,4,-1,1]
+Output: 2
+Explanation: 1 is in the array but 2 is missing.
+'''
+'''
+Approach:- Hashing key
+TC:- O(n)
+Sc:- O(1)
+'''
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        nums.append(0)
+        n = len(nums)
+        for i in range(len(nums)): #delete those useless elements
+            if nums[i]<0 or nums[i]>=n:
+                nums[i]=0
+        for i in range(len(nums)): #use the index as the hash to record the frequency of each number
+            nums[nums[i]%n]+=n
+        for i in range(1,len(nums)):
+            if nums[i]//n==0:
+                return i
+        return n

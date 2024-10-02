3a7b19617f3baad137b53f4b0b597964f6386dc4
shreyat <arjenbhar@gmail.com>
2024-10-02 11:34:12 +0530

    Check32_64.c : Adding comment to enable Macro
    Adding Macro for Debugging

--- Apple/Check32_64.c
+++ Apple/Check32_64.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 int main(void)
 {
     if (sizeof(void *) == 4)
-    { printf("32\n"); }
+    {
+      #ifdef DEBUG
+        printf("32\n");
+      #endif 
+    }
     if (sizeof(void*) == 8)
-    { printf ("64\n"); }
+    { 
+        #ifdef DEBUG
+          printf ("64\n"); 
+        #endif
+     }
     return 0;
 }
+/* 
+  To enable Macro
+  gcc Check32_64.c -DDEBUG -o test
+*/

0ca583cee1aab5f41c2d8b5bfac07fea52d2de83
shreyat <arjenbhar@gmail.com>
2024-10-28 17:19:13 +0530

    Removing indent spaces

--- Meta/Practice/Python/Cutting Ribbons.py
+++ Meta/Practice/Python/Cutting Ribbons.py
@@ -37,7 +37,7 @@ class Solution:
         if sumOfRibbons < k:
             return 0
       
-       def isBisect(target):
+        def isBisect(target):
             nonlocal ribbons,k
             count = 0
             for rib in ribbons:
@@ -58,7 +58,7 @@ class Solution:
         return right
       
       
- '''
+'''
 Input:- ribbons = [9,7,5], k = 3
 Output: 5
 
@@ -67,4 +67,5 @@ Explanation:
 - Cut the second ribbon to two ribbons, one of length 5 and one of length 2.
 - Keep the third ribbon as it is.
 Now you have 3 ribbons of length 5.
- '''
+
+'''

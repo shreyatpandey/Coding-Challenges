724d8b2c52933e65b0e67f7af5b8e964f9435f50
shreyat <arjenbhar@gmail.com>
2024-10-29 17:06:43 +0530

    Adding Print Function

--- C/BitManipulation/Swap-2-Bits-At-A-Given-Position.c
+++ C/BitManipulation/Swap-2-Bits-At-A-Given-Position.c
@@ -1,7 +1,9 @@
 
-#include <iostream>
-#include <bitset>
-using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#define MASK_8_BIT 0x80
 // Function to swap bits at position `p` and `q` in integer `n`
 int swap(int n, int p, int q)
 {
@@ -13,15 +15,31 @@ int swap(int n, int p, int q)
     }
     return n;
 }
+
+void print(int value) 
+{
+    for (int i = 0; i<8; i++) {
+        if (value & (MASK_8_BIT >> i)) {
+            printf("1");
+        }
+        else {
+            printf("0");
+        }
+    }
+    printf("\n");
+}
  
 int main()
 {
-    int n = 31;
-    int p = 2, q = 6;    // swap 3rd and 7th bit from the right
- 
-    cout << n << " in binary is " << bitset<8>(n) << endl;
+    int n = 5;
+    int p = 1, q = 2;    // swap 3rd and 7th bit from the right
+    
+    printf("Value Before Swap : \n");
+    print(n);
     n = swap (n, p, q);
-    cout << n << " in binary is " << bitset<8>(n) << endl;
- 
+
+    printf("Value After Swap : \n");
+    print(n);
+
     return 0;
 }

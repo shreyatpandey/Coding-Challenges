3635c90233fea094f85a366d142495f46defad34
shreyat <arjenbhar@gmail.com>
2024-11-01 18:08:36 +0530

    Reading-From-Binary-File.c

---
+++ C/Reading-From-Binary-File.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <errno.h>
+
+typedef struct Point {
+    int x, y;
+} Point;
+
+int main() {
+    Point p1 = {
+        .x = 13,
+        .y = -1
+    };
+    Point p2;
+    
+    FILE *in;
+    FILE *out;
+
+    // WRITE 
+    out = fopen( "Point.bin", "wb");
+    if (out == NULL) {
+        return -EINVAL;
+    }
+
+
+    size_t bytes_written = fwrite(&p1, sizeof(Point), 1, out);
+    fclose(out);
+    if (bytes_written == 0) {
+        return -1;
+    }
+
+    // READ
+    in = fopen("Point.bin", "rb");
+    if (in == NULL) {
+        return -2;
+    }
+
+    size_t element_read = fread(&p2, sizeof(Point),1, in);
+    fclose(in);
+    if (element_read == 0) {
+        return -3;
+    }
+
+    printf("Pont read from binary file : %d, %d \n", p2.x, p2.y);
+
+    return 0;
+
+}

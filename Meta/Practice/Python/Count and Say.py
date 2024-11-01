a6e3eb5642387402a10e6d452f06785069047092
shreyat <arjenbhar@gmail.com>
2024-11-01 19:33:56 +0530

    Adding Problem Discription

--- Meta/Practice/Python/Count and Say.py
+++ Meta/Practice/Python/Count and Say.py
@@ -1,4 +1,40 @@
 '''
+The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
+
+countAndSay(1) = "1"
+countAndSay(n) is the run-length encoding of countAndSay(n - 1).
+
+Given a positive integer n, return the nth element of the count-and-say sequence.
+'''
+'''
+Example 1:
+
+Input: n = 4
+
+Output: "1211"
+
+Explanation:
+
+countAndSay(1) = "1"
+countAndSay(2) = RLE of "1" = "11"
+countAndSay(3) = RLE of "11" = "21"
+countAndSay(4) = RLE of "21" = "1211"
+
+Example 2:
+
+Input: n = 1
+
+Output: "1"
+
+Explanation:
+
+This is the base case.
+'''
+'''
+Constraints:
+1 <= n <= 30
+'''
+'''
 TC:- O(n^2)
 SC:- O(n)
 '''

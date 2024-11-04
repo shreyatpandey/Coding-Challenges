971b862bae13056d04d4795c7f0a8dd38f9e48eb
shreyat <arjenbhar@gmail.com>
2024-11-04 09:41:17 +0530

    Nit : Adding Trace

--- Meta/Practice/Python/Non-LC-Tagged/Sorted-Iterator.py
+++ Meta/Practice/Python/Non-LC-Tagged/Sorted-Iterator.py
@@ -28,7 +28,9 @@ class SortedIterator:
     def next(self):
         if not self.minh:
             return None
+        print("Self.minh", self.minh)
         num, i, j = heapq.heappop(self.minh)
+        print("num : ", num)
         if j+1 < len(self.lists[i]):
             heapq.heappush(self.minh, (self.lists[i][j+1], i, j+1))
         return num
@@ -41,3 +43,43 @@ if __name__ == "__main__":
     s = SortedIterator([[1, 4, 5, 8, 9], [3, 4, 4, 6], [0, 2, 8]])
     while s.hasNext():
         print(s.next())
+
+'''
+Output : Trace
+Self.minh [(0, 2, 0), (3, 1, 0), (1, 0, 0)]
+num :  0
+0
+Self.minh [(1, 0, 0), (3, 1, 0), (2, 2, 1)]
+num :  1
+1
+Self.minh [(2, 2, 1), (3, 1, 0), (4, 0, 1)]
+num :  2
+2
+Self.minh [(3, 1, 0), (4, 0, 1), (8, 2, 2)]
+num :  3
+3
+Self.minh [(4, 0, 1), (8, 2, 2), (4, 1, 1)]
+num :  4
+4
+Self.minh [(4, 1, 1), (8, 2, 2), (5, 0, 2)]
+num :  4
+4
+Self.minh [(4, 1, 2), (8, 2, 2), (5, 0, 2)]
+num :  4
+4
+Self.minh [(5, 0, 2), (8, 2, 2), (6, 1, 3)]
+num :  5
+5
+Self.minh [(6, 1, 3), (8, 2, 2), (8, 0, 3)]
+num :  6
+6
+Self.minh [(8, 0, 3), (8, 2, 2)]
+num :  8
+8
+Self.minh [(8, 2, 2), (9, 0, 4)]
+num :  8
+8
+Self.minh [(9, 0, 4)]
+num :  9
+9
+'''

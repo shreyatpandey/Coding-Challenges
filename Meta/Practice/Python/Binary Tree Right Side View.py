f2bee6ff5baaebff403f2fe18b191c7c86cbdb4d
shreyat <arjenbhar@gmail.com>
2024-11-07 16:13:44 +0530

    Nit : Adding Question

--- Meta/Practice/Python/Binary Tree Right Side View.py
+++ Meta/Practice/Python/Binary Tree Right Side View.py
@@ -4,7 +4,23 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+'''
+Input: root = [1,2,3,null,5,null,4]
+Output: [1,3,4]
+Example 2:
 
+Input: root = [1,null,3]
+Output: [1,3]
+Example 3:
+
+Input: root = []
+Output: []
+'''
+'''
+Constraints:
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+'''
 '''
 Approach-1: Using deque
 RunTime:- 98.01% fast

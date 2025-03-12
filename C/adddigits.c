79b581c92732a58bae28808edca0d680c5d54389
shreyat <arjenbhar@gmail.com>
2025-03-12 09:35:24 +0530

    Fix : Add Digits

--- C/adddigits.c
+++ C/adddigits.c
@@ -8,21 +8,16 @@ int main()
    int m=no;
    int sum=0;
    int remainder=0;
-   while(no!=0 || sum>9)
-{
-  if(no==0)
-   {
-     no = sum;
-     sum =0;
-   }
-   
+   while(no!=0 || sum>9) {
+    if(no==0) {
+      no = sum;
+      sum =0;
+    }
    remainder = no%10;
    sum+=remainder;
    no=no/10;
    
 }
-printf("The required final digit is:%d\n",sum);
-
-
-return 0;
+  printf("The required final digit is:%d\n",sum);
+  return 0;
 }

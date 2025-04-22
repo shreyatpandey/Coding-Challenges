e12686c81a7974de9abd316ee3b341fdea6dec90
shreyat <arjenbhar@gmail.com>
2025-04-22 11:32:24 +0530

    Movie-Rating : Adding more test cases

--- Amazon/OA-Sample/IMDB-Movie-Rating/Movie-Rating.py
+++ Amazon/OA-Sample/IMDB-Movie-Rating/Movie-Rating.py
@@ -27,11 +27,8 @@ def max_ascents(ratings):
   return ascents
 
 # Test Cases
-test_cases = [
-    ([2, 1, 3], 3, 2),
-    ([2, 1, 1, 2], 4, 2),
-]
 if __name__ == "__main__":
   print(max_ascents([2, 1, 3])) # Output : 2
   print(max_ascents([2, 1, 1, 2])) # Output: 2
   print(max_ascents([2,3,1,5,4])) # Output: 4
+  print(max_ascents([5,5,5,5]))

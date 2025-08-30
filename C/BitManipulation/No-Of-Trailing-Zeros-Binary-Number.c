03dc1997791c707aec79d67ed0e5c94b8719e9f5
shreyat <arjenbhar@gmail.com>
2025-08-30 12:37:01 +0530

    BitManipulation: Modified Bug fix
    Previous function to calculate trailing zeros was incorrect.
    Fixed it.

--- C/BitManipulation/No-Of-Trailing-Zeros-Binary-Number.c
+++ C/BitManipulation/No-Of-Trailing-Zeros-Binary-Number.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Definition of Trailing Zeros */
+/* Trailing zeros in a binary number are the consecutive zeros that appear at the end (rightmost side) of the binary representation of the number. */
+
 int trailing_zeros(unsigned int num) {
     if (num == 0) {
         return 32; // All 32 bits are zeros
     }
     int result_count = 0;
-    while (num & 1) {
+    while ((num & 1) == 0) {
         result_count++;
         num >>= 1;
     }
@@ -22,3 +25,9 @@ int main (int argc, char *argv[]) {
 
     return 0;
 }
+/*
+ For example:
+ For binary number 1000 (8 in decimal), it will return 3
+ For binary number 1010 (10 in decimal), it will return 1
+ For binary number 1011 (11 in decimal), it will return 0
+*/

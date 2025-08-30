e65883a8cf9c518d596578f26a659ddc5343f5d2
shreyat <arjenbhar@gmail.com>
2025-08-30 12:17:35 +0530

    BitManipulation: Add function to swap endianness                  of 16-bit integers
    Added new function to swap endianness of 16-bit integers
    and updated main function to demonstrate its usage.

--- C/BitManipulation/Convert-Endianness.c
+++ C/BitManipulation/Convert-Endianness.c
@@ -9,8 +9,15 @@ uint32_t swap_endianness(uint32_t num) {
            ((num << 24) & 0xFF000000);  // Move byte 0 to byte 3
 }
 
+// Function to swap the endianness of a 16-bit integer
+uint16_t swap_endianness_16(uint16_t num) {
+    return ((num >> 8) & 0x00FF) | // Move byte 1 to byte 0
+           ((num << 8) & 0xFF00);  // Move byte 0 to byte 1
+}
+
 int main() {
     uint32_t num;
+    uint16_t num16;
 
     printf("Enter a 32-bit integer in hexadecimal (e.g., 0x12345678): ");
     scanf("%x", &num);
@@ -19,6 +26,13 @@ int main() {
 
     uint32_t swapped = swap_endianness(num);
     printf("Number after swapping endianness: 0x%08X\n", swapped);
+
+    printf("Enter a 16-bit integer in hexadecimal (e.g., 0x1234): ");
+    scanf("%hx", &num16);
+
+    printf("Original 16-bit number: 0x%04X\n", num16);
+    uint16_t swapped16 = swap_endianness_16(num16);
+    printf("16-bit number after swapping endianness: 0x%04X\n", swapped16);
 
     return 0;
 }

41cb71c75dfec06184a2864b9bc7045ad1246330
shreyat <arjenbhar@gmail.com>
2025-09-20 12:13:23 +0530

    Java: Adding these files
    1. Remove-Duplicates.java
    2. Search-In-Rotated-Sorted-Array.java
    3. TwoSum.java

---
+++ Java/Remove-Duplicates.java
@@ -0,0 +1,26 @@
+package Java;
+
+class RemoveDuplicates {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) return 0;
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        RemoveDuplicates solution = new RemoveDuplicates();
+        int[] nums = {0,0,1,1,1,2,2,3,3,4};
+        int length = solution.removeDuplicates(nums);
+        System.out.println("New length: " + length);
+        System.out.print("Array after removing duplicates: ");
+        for (int i = 0; i < length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
---
+++ Java/TwoSum.java
@@ -0,0 +1,26 @@
+package Java;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+    public static void main(String[] args) {
+        TwoSum solution = new TwoSum();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = solution.twoSum(nums, target);
+        System.out.println("Indices: " + result[0] + ", " + result[1]);
+    }
+}

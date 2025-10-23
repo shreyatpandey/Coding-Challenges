f84a294b1a411f1c82c5d2a7747e73ee53a82008
shreyat <arjenbhar@gmail.com>
2025-10-23 17:32:57 +0530

    String-Compression: Make code improvements
    Make improvements to the String-Compression.c file by removing
    unused variable and ensuring proper function structure.
    Added Test Cases as well

--- C/Strings/String-Compression.c
+++ C/Strings/String-Compression.c
@@ -4,13 +4,11 @@
 
 
 char* encode(char* src) 
-{ 
-    
-    char count[MAX_LEN]; 
+{
     int len = strlen(src); 
     char* dest = (char*)malloc(sizeof(char) * (len * 2 + 1)); 
   
-    int i, j = 0, k; 
+    int i = 0;
     int anchor = 0;
     int write = 0;
     /* traverse the input string one by one */
@@ -34,21 +32,12 @@ char* encode(char* src)
                     dest[write++] = anchor + '0'; 
             }
        
-       anchor = 0;
-    }
+            anchor = 0;
+        }
     }
     dest[write] = '\0';
     return dest; 
-} 
-  
-/*driver program to test above function */
-int main() 
-{ 
-    char str[] = "a"; 
-    char* res = encode(str); 
-    printf("%s", res); 
-    getchar(); 
-} 
+}
 
 //Approach-2 :
 //TC:- O(N^2)
@@ -88,8 +77,18 @@ char* RunLengthEncoding(const char* Input)
 
 int main()
 {
+    printf("Test Case-1:\n");
     const char * Input = "aaaaaaaaaabb" ;
+    char *Result = encode((char*)Input);
     char* ResultString = RunLengthEncoding(Input);
+    printf("Encoded String : %s\n", Result);
+    printf("Encoded String Approach-2 : %s\n", ResultString);
+
+    printf("Test Case-2:\n");
+    char Input2[] = "a";
+    char *Result2 = encode((char*)Input2);
+    printf("Encoded String : %s\n", Result2);
+
     return 0;
 }
 

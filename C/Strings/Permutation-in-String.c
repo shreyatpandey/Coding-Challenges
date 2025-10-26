8ee889b7fa89ffe5a7e848cdf3a4a75e56c4c8f7
shreyat <arjenbhar@gmail.com>
2025-10-26 09:12:00 +0530

    String: Add header files for Permutation-in-String.c

--- C/Strings/Permutation-in-String.c
+++ C/Strings/Permutation-in-String.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
 /*Given two strings s1 and s2, write a function to return true if s2 contains the permutation of s1. 
 In other words, one of the first string's permutations is the substring of the second string.
 Input: s1 = "ab" s2 = "eidbaooo"
